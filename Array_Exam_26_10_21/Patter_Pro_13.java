ad2febab7da74ceb396067d7f8350a9c28b8c9c6
manonmani <manonmani.pandukumar@gmail.com>
2021-10-26 22:28:33 +0530

    Program

---
+++ Array_Exam_26_10_21/Patter_Pro_13.java
@@ -0,0 +1,24 @@
+package Array_Exam_26_10_21;
+
+public class Patter_Pro_13 {
+public static void main(String[] args) {
+	int x=1;
+	for(int i=1;i<=5;i++)
+	{
+		
+		for(int j=1;j<=5;j++)
+		{
+			System.out.print(x+"   ");
+			x=x+1;
+		}
+		System.out.println();
+	}
+}
+}
+/*
+1     2     3     4     5     
+6     7     8     9     10     
+11     12     13     14     15     
+16     17     18     19     20     
+21     22     23     24     25   
+*/
