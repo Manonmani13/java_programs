8498a7a2c13f7725495aa561075d4bff661f2349
manonmani <manonmani.pandukumar@gmail.com>
2021-10-26 22:32:53 +0530

    Program

---
+++ Array_Exam_26_10_21/Pattern_20.java
@@ -0,0 +1,14 @@
+package Array_Exam_26_10_21;
+
+public class Pattern_20 {
+public static void main(String[] args) {
+	for(int i=1;i<=5;i++)
+	{
+		for(int j=1;j<=5;j++)
+		{
+			System.out.print((i*j)%2+" ");
+		}
+		System.out.println();
+	}
+}
+}
