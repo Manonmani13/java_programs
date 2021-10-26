caea5d0744724933df5843b0978ea5bf3fbf7d09
manonmani <manonmani.pandukumar@gmail.com>
2021-10-27 00:03:25 +0530

    Program

---
+++ Operators/Arith_wx1.java
@@ -0,0 +1,9 @@
+package Operators;
+
+public class Arith_wx1 {
+	public static void main(String args[]){  
+		System.out.println(10*10/5+3-1*4/2);  //100/5=20 1*4=4/2=2
+		//20+3=23-2=21
+		System.out.println(10*10/5);
+		}
+}
---
+++ Operators/Ternary2.java
@@ -0,0 +1,11 @@
+package Operators;
+
+public class Ternary2 {
+	public static void main(String args[]){  
+		int a=10;  
+		int b=5;  
+		int min=(a<b)?a:b;  
+		System.out.println(min);  
+		}
+}
+//5
