abcee46bed2869960f95b08fddfca30d287a33e3
manonmani <manonmani.pandukumar@gmail.com>
2021-10-28 00:26:44 +0530

    program

---
+++ Array_27_10_21/String_Exa_3.java
@@ -0,0 +1,15 @@
+package Array_27_10_21;
+
+public class String_Exa_3 {
+public static void main(String[] args) {
+	String n1=new String("Sathish");
+	System.out.println(n1.hashCode());
+	String n2=new String("Sathish");
+	System.out.println(n2.hashCode());
+}
+}
+/*
+ * -759246884
+-759246884
+
+ */
