cc527138453c39fcb1e9c2f0b68c4eaf71c5485d
manonmani <manonmani.pandukumar@gmail.com>
2021-10-27 22:43:58 +0530

    Program

---
+++ Array_27_10_21/Constructor.java
@@ -0,0 +1,16 @@
+package Array_27_10_21;
+
+public class Constructor {
+	Constructor()
+	{
+		
+	}
+	void Constructor()//constructor cannot be override
+	{
+		
+	}
+public static void main(String[] args) {
+
+	
+}
+}
