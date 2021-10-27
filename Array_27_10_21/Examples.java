c25ff5ec33de5d473bc6777ec99c5212eaa92473
manonmani <manonmani.pandukumar@gmail.com>
2021-10-28 00:24:33 +0530

    program

---
+++ Array_27_10_21/Examples.java
@@ -0,0 +1,11 @@
+package Array_27_10_21;
+
+public class Examples {
+public static void main(String[] args) {
+	int a[]=new int[-4];
+	//System.out.println(a.length);error
+	int b[][]=new int[4][0];
+	System.out.println(b.length);
+	System.out.println(b[0].length);
+}
+}
