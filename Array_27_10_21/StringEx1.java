5b01a8fa07cdceb9ef17a9f0870dc2127a527eff
manonmani <manonmani.pandukumar@gmail.com>
2021-10-28 00:27:10 +0530

    program

---
+++ Array_27_10_21/StringEx1.java
@@ -0,0 +1,10 @@
+package Array_27_10_21;
+
+public class StringEx1 {
+public static void main(String[] args) {
+	char c[]= {'h','e','l','l','o'};
+	String s=new String(c);
+	System.out.println(c);
+}
+}
+//hello
