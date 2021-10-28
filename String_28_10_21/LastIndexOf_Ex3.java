e7005a8b722581bf7e70647447137cebb8a21dd6
manonmani <manonmani.pandukumar@gmail.com>
2021-10-28 23:23:08 +0530

    last Index of

---
+++ String_28_10_21/LastIndexOf_Ex3.java
@@ -0,0 +1,10 @@
+package String_28_10_21;
+
+public class LastIndexOf_Ex3 {
+	public static void main(String[] args) {           
+        String str = "This is last index of example";  
+        int index = str.lastIndexOf("of");  
+        System.out.println(index);        
+    }  
+}
+//19
