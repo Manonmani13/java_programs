d2b7f2bd41704a2b58206d08a973ea8405fccfea
manonmani <manonmani.pandukumar@gmail.com>
2021-10-28 23:04:14 +0530

    Index of

---
+++ String_28_10_21/IndexOf_Ex6.java
@@ -0,0 +1,11 @@
+package String_28_10_21;
+
+public class IndexOf_Ex6 {
+	public static void main(String[] args) {          
+        String s1 = "This is indexOf method";         
+        // Passing char and index from  
+        int index = s1.indexOf('e', 12); //Returns the index of this char  
+        System.out.println("index of char "+index);       
+    } 
+}
+//index of char 17
