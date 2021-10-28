11a7e2efe71b0cb2cff8ccd5476fc12595527207
manonmani <manonmani.pandukumar@gmail.com>
2021-10-28 23:20:08 +0530

    Index of

---
+++ String_28_10_21/IndexOf_Ex5.java
@@ -0,0 +1,14 @@
+package String_28_10_21;
+
+public class IndexOf_Ex5 {
+	public static void main(String[] args) {          
+        String name = "manonmani";         
+       char key='a';
+        System.out.println("index of char "+name.indexOf(key));   
+        System.out.println("last index"+name.lastIndexOf(key));
+    } 
+}
+/*index of char 1
+last index6
+
+*/
