cf7bf2cb1646a011fea8dec4b3c668412433c89a
manonmani <manonmani.pandukumar@gmail.com>
2021-10-28 23:01:42 +0530

    char at

---
+++ String_28_10_21/IndexOf_Ex1.java
@@ -0,0 +1,23 @@
+package String_28_10_21;
+
+public class IndexOf_Ex1 {
+	public static void main(String args[]){  
+		String s1="this is index of example";  
+		 
+		int index1=s1.indexOf("is");
+		int index2=s1.indexOf("index"); 
+		System.out.println(index1+"  "+index2);//2 8  
+		  
+		 
+		int index3=s1.indexOf("is",4);  
+		System.out.println(index3);
+		
+		int index4=s1.indexOf('s');
+		System.out.println(index4);//3  
+
+}
+}/*
+2  8
+5
+3
+*/
