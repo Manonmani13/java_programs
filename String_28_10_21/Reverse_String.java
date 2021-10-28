03e288bc605a98d2845f7eb720c02c14f1923cee
manonmani <manonmani.pandukumar@gmail.com>
2021-10-28 23:50:55 +0530

    reverse

---
+++ String_28_10_21/Reverse_String.java
@@ -0,0 +1,12 @@
+package String_28_10_21;
+
+public class Reverse_String {
+public static void main(String[] args) {
+	String name="manonmani";
+	for(int i=name.length()-1;i>=0;i--)
+	{
+		System.out.print(name.charAt(i)+" ");
+	}
+}
+}
+//i n a m n o n a m 
