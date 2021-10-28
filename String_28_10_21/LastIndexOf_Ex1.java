c194a9e4ae375eac0a9d5eaa5b09acbe061b1b22
manonmani <manonmani.pandukumar@gmail.com>
2021-10-28 23:21:44 +0530

    last Index of

---
+++ String_28_10_21/LastIndexOf_Ex1.java
@@ -0,0 +1,9 @@
+package String_28_10_21;
+
+public class LastIndexOf_Ex1 {
+	public static void main(String args[]){  
+		String s1="this is index of example";  
+		int index1=s1.lastIndexOf('s'); 
+		System.out.println(index1);//6  
+		}
+}
