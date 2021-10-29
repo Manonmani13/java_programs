a58566244d94d7ce10e88956ba632e99192c2530
manonmani <manonmani.pandukumar@gmail.com>
2021-10-29 22:55:46 +0530

    pattern

---
+++ String_method_29_10_21/Pattern_20.java
@@ -0,0 +1,22 @@
+package String_method_29_10_21;
+
+public class Pattern_20 {
+public static void main(String[] args) {
+	int n=5;
+	for(int i=1;i<=5;i++)
+	{
+		for(int j=1;j<=5;j++)
+		{
+			System.out.print((i*j)%2+" ");
+		}
+		System.out.println();
+	}
+}
+}
+/*
+1 0 1 0 1 
+0 0 0 0 0 
+1 0 1 0 1 
+0 0 0 0 0 
+1 0 1 0 1 
+*/
