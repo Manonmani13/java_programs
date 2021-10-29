7d49c9379b62a333ba7d4470be5c725f6bf5b981
manonmani <manonmani.pandukumar@gmail.com>
2021-10-29 22:46:09 +0530

    pattern

---
+++ String_method_29_10_21/Patt.java
@@ -0,0 +1,21 @@
+package String_method_29_10_21;
+
+public class Patt {
+	public static void main(String[] args) {
+		for(int i=0;i<5;i++)
+		{
+			for(int j=1;j<=5;j++)
+			{
+				System.out.print((i%2)+" ");
+			}
+			System.out.println();
+		}
+	}
+}
+/*
+0 0 0 0 0 
+1 1 1 1 1 
+0 0 0 0 0 
+1 1 1 1 1 
+0 0 0 0 0 
+*/
