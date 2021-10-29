8a8f1cdea12b9a1f5d3de7eb279b72afb93137a4
manonmani <manonmani.pandukumar@gmail.com>
2021-10-29 22:56:26 +0530

    pattern

---
+++ String_method_29_10_21/Patter_24.java
@@ -0,0 +1,21 @@
+package String_method_29_10_21;
+
+public class Patter_24 {
+public static void main(String[] args) {
+	for(int i=1;i<=5;i++)
+	{
+		for(int j=1;j<=5;j++)
+		{
+			System.out.print(j%2+" ");
+		}
+		System.out.println( );
+	}
+}
+}
+/*
+0 0 0 0 0 
+1 1 1 1 1 
+0 0 0 0 0 
+1 1 1 1 1 
+0 0 0 0 0 
+*/
