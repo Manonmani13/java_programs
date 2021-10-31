bf2c09fd6ee0ae40a439efc7dc66a8228a37d0aa
manonmani <manonmani.pandukumar@gmail.com>
2021-10-31 22:50:28 +0530

    date time Programs

---
+++ Date_Time/Current_Date_time.java
@@ -0,0 +1,11 @@
+package Date_Time;
+
+import java.time.LocalDateTime;
+
+public class Current_Date_time {
+public static void main(String[] args) {
+	LocalDateTime obj=LocalDateTime.now();
+	System.out.println(obj);
+}
+}
+//2021-10-31T14:41:57.879035
