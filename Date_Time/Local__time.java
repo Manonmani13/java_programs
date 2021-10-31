d0434ee491adcc4504c8daaf3fbb3ce99db8e5f1
manonmani <manonmani.pandukumar@gmail.com>
2021-10-31 22:53:49 +0530

    date time Programs

---
+++ Date_Time/Local__time.java
@@ -0,0 +1,11 @@
+package Date_Time;
+
+import java.time.LocalTime;
+
+public class Local__time {
+public static void main(String[] args) {
+LocalTime obj=LocalTime.now();
+System.out.println(obj);
+}
+}
+//14:38:15.983432
