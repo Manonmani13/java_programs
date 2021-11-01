45253ba35d90e70cf72ff81b2b240751b2db6ff2
manonmani <manonmani.pandukumar@gmail.com>
2021-11-01 20:27:04 +0530

    number

---
+++ Interview_Program_1_11_21/Number_Pro1.java
@@ -0,0 +1,23 @@
+package Interview_Program_1_11_21;
+
+public class Number_Pro1 {
+public static void main(String[] args) {
+	for(int i=1;i<=7;i++)
+	{
+		for(int j=7;j>=i;j--)
+		{
+			System.out.print(j+" ");
+		}
+		System.out.println();
+	}
+}
+}
+/*
+7 6 5 4 3 2 1 
+7 6 5 4 3 2 
+7 6 5 4 3 
+7 6 5 4 
+7 6 5 
+7 6 
+7 
+*/
