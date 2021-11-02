2119fb8c5a57c193bb855bfec99269cae56adaaa
manonmani <manonmani.pandukumar@gmail.com>
2021-11-02 21:59:00 +0530

    date and time

---
+++ Date_Time_2_11_21/ZonedDateTime_Ex3.java
@@ -0,0 +1,11 @@
+package Date_Time_2_11_21;
+
+import java.time.ZonedDateTime;
+
+public class ZonedDateTime_Ex3 {
+	public static void main(String[] args) {  
+	    ZonedDateTime zone =ZonedDateTime.now();  
+	    System.out.println(zone.getZone());  
+	  }  
+}
+//Asia/Kolkata
