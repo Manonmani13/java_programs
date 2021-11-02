55ce3d3a0bc0c792377780c550629256268575e6
manonmani <manonmani.pandukumar@gmail.com>
2021-11-02 21:56:49 +0530

    date and time

---
+++ Date_Time_2_11_21/OffSetTime_Ex4.java
@@ -0,0 +1,13 @@
+package Date_Time_2_11_21;
+
+import java.time.OffsetTime;
+
+public class OffSetTime_Ex4 {
+	public static void main(String[] args) {
+		 OffsetTime of = OffsetTime.now();  
+		    int s = of.getSecond();  
+		    System.out.println(s );
+	}
+   
+}
+//52
