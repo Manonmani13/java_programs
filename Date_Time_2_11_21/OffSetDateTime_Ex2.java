b5fdd8b152461a0e35612c09d014c32b6fe82193
manonmani <manonmani.pandukumar@gmail.com>
2021-11-02 21:52:46 +0530

    date and time

---
+++ Date_Time_2_11_21/OffSetDateTime_Ex2.java
@@ -0,0 +1,11 @@
+package Date_Time_2_11_21;
+
+import java.time.OffsetDateTime;
+
+public class OffSetDateTime_Ex2 {
+public static void main(String[] args) {
+	 OffsetDateTime offsetDT = OffsetDateTime.now();  
+     System.out.println(offsetDT.getDayOfYear());  
+}
+}
+//306
