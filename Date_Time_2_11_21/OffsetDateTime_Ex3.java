484841a6c3dc99b3609febc6d2bf5d7ff9f56621
manonmani <manonmani.pandukumar@gmail.com>
2021-11-02 21:53:35 +0530

    date and time

---
+++ Date_Time_2_11_21/OffsetDateTime_Ex3.java
@@ -0,0 +1,11 @@
+package Date_Time_2_11_21;
+
+import java.time.OffsetDateTime;
+
+public class OffsetDateTime_Ex3 {
+	 public static void main(String[] args) {  
+	        OffsetDateTime offsetDT = OffsetDateTime.now();  
+	        System.out.println(offsetDT.getDayOfWeek());  
+	    }  
+}
+//TUESDAY
