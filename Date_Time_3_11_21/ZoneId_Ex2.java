efb58adba7792e9f3424ffdf04c2b7adc3c3e7ec
manonmani <manonmani.pandukumar@gmail.com>
2021-11-03 23:32:51 +0530

    Date and time

---
+++ Date_Time_3_11_21/ZoneId_Ex2.java
@@ -0,0 +1,9 @@
+package Date_Time_3_11_21;
+import java.time.*;
+public class ZoneId_Ex2 {
+	 public static void main(String[] args) {  
+		    ZoneId zone = ZoneId.systemDefault();     
+		    System.out.println(zone);  
+		  } 
+}
+//Asia/Kolkata
