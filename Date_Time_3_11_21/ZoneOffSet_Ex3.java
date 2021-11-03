f179453d35dd04a53823e4adf80d2e6ccacdc89a
manonmani <manonmani.pandukumar@gmail.com>
2021-11-03 23:34:18 +0530

    Date and time

---
+++ Date_Time_3_11_21/ZoneOffSet_Ex3.java
@@ -0,0 +1,9 @@
+package Date_Time_3_11_21;
+import java.time.ZoneOffset;  
+public class ZoneOffSet_Ex3 {
+	public static void main(String[] args) {  
+	    ZoneOffset zone = ZoneOffset.ofHoursMinutes(5,30);  
+	    System.out.println(zone);  
+	  }  
+}
+//+05:30
