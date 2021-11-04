4e95f4ca85f94171cc3e15471808797bef9cc1ff
manonmani <manonmani.pandukumar@gmail.com>
2021-11-04 21:26:27 +0530

    game

---
+++ Game_3_11_21/Demo.java
@@ -0,0 +1,41 @@
+package Game_3_11_21;
+
+import java.util.Scanner;
+
+public class Demo {
+
+public static void main(String[] args) {
+		
+		Boolean boo=false;
+		int n=3;
+		int v=3;
+		int a[]=new int[n];
+		int b[]=new int[v];
+		char ch[]=new char[v];
+		Scanner sc=new Scanner(System.in);
+		for(int i=0;i<n;i++)a[i]=sc.nextInt();
+		for(int j=0;j<v;j++) b[j]=sc.nextInt();
+		for(int i=0;i<n;i++)
+		{
+			for(int j=0;j<v;j++) {
+				if(a[i]==b[j]) {
+					ch[j]= 'C';
+				}
+				else if (a[i]==b[j] && i!=j) {
+					
+					ch[j]='P';
+					b[i]=a[j];
+				}
+				else {
+					ch[j]='X';
+					boo=true;
+				}
+			}
+		}
+		for (char c: ch) {
+			System.out.println(c);
+		}
+		System.out.println("jj");
+	}
+
+}
