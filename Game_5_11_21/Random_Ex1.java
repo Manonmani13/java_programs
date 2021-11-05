6759915c09ceb89d908072cab6723d211e0e9c97
manonmani <manonmani.pandukumar@gmail.com>
2021-11-05 21:20:51 +0530

    random

---
+++ Game_5_11_21/Random_Ex1.java
@@ -0,0 +1,19 @@
+package Game_5_11_21;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Random_Ex1 {
+public static void main(String[] args) {
+	Random r=new Random();
+	System.out.println(r);
+	int rand=r.nextInt();
+	System.out.println(rand);
+	
+	
+}
+}
+/*
+java.util.Random@6ff3c5b5
+-128180294
+*/
