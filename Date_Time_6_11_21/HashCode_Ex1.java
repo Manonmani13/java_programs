239b2b09326b42777108e3cb9c86f22ece407d4f
manonmani <manonmani.pandukumar@gmail.com>
2021-11-06 17:03:00 +0530

    date and time

---
+++ Date_Time_6_11_21/HashCode_Ex1.java
@@ -0,0 +1,12 @@
+package Date_Time_6_11_21;
+
+import java.util.Date;
+
+public class HashCode_Ex1 {
+	public static void main(String[] args) {
+		Date d=new Date(1999,6,11);
+		
+		System.out.println(d.hashCode());
+	}
+}
+//110458657
