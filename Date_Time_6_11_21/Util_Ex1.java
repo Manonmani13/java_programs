96a8eaf98eecbe299c6a4d28b445d3617860a818
manonmani <manonmani.pandukumar@gmail.com>
2021-11-06 17:26:20 +0530

    date and time

---
+++ Date_Time_6_11_21/Util_Ex1.java
@@ -0,0 +1,9 @@
+package Date_Time_6_11_21;
+
+public class Util_Ex1 {
+public static void main(String[] args) {
+	java.util.Date date=new java.util.Date();
+	System.out.println(date);
+}
+}
+//Sat Nov 06 13:28:18 IST 2021
