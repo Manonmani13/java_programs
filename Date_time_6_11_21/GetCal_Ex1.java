dbe513f7fa28c7da29a2cbfba0b867477802a479
manonmani <manonmani.pandukumar@gmail.com>
2021-11-06 22:08:56 +0530

    date and time

---
+++ Date_time_6_11_21/GetCal_Ex1.java
@@ -0,0 +1,14 @@
+package Date_time_6_11_21;
+
+import java.util.Calendar;
+
+public class GetCal_Ex1 {
+	public static void main(String[] args) {  
+		 // creating a calendar named  as cal1  
+		Calendar calinstance = Calendar.getInstance();  
+		//  Print the calendar type of calinstance instance of calendar class  
+		  System.out.println("Type of Calendar is " +  
+		  calinstance.getCalendarType());  
+		     }  
+}
+//Type of Calendar is gregory
