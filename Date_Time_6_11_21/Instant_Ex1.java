a23d9e3784e1a5b76fad289d191cc966d537c60b
manonmani <manonmani.pandukumar@gmail.com>
2021-11-06 17:06:13 +0530

    date and time

---
+++ Date_Time_6_11_21/Instant_Ex1.java
@@ -0,0 +1,11 @@
+package Date_Time_6_11_21;
+
+import java.time.Instant;
+
+public class Instant_Ex1 {
+public static void main(String[] args) {
+	Instant inst=Instant.parse("2017-02-02T03:20:50.00Z");
+	System.out.println(inst);
+}
+}
+//2017-02-02T03:20:50Z
