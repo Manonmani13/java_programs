cb1bf088b6a2858e77f3de254fe8a76a9b15d262
manonmani <manonmani.pandukumar@gmail.com>
2021-11-06 17:19:53 +0530

    date and time

---
+++ Date_Time_6_11_21/SQLDateEx_2.java
@@ -0,0 +1,12 @@
+package Date_Time_6_11_21;
+
+import java.sql.Date;
+
+public class SQLDateEx_2 {
+public static void main(String[] args) {
+	String str="2015-02-31";
+	Date date=Date.valueOf(str);
+	System.out.println(date);
+}
+}
+//2015-03-03
