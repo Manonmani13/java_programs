0b4dcf04107c7477c384e3e45901b392cbbf0ddb
manonmani <manonmani.pandukumar@gmail.com>
2021-11-06 17:19:00 +0530

    date and time

---
+++ Date_Time_6_11_21/SQLDateEx_1.java
@@ -0,0 +1,12 @@
+package Date_Time_6_11_21;
+
+public class SQLDateEx_1 {
+	
+	public static void main(String[] args) {
+		long mills=System.currentTimeMillis();
+		java.sql.Date date=new java.sql.Date(mills);
+		System.out.println(date);
+	}
+	}
+	//2021-11-06
+
