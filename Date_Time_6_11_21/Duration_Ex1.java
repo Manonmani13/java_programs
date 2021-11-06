72554e98a2531eb95c3cc5d728cba1fc818c7ead
manonmani <manonmani.pandukumar@gmail.com>
2021-11-06 16:56:45 +0530

    date and time

---
+++ Date_Time_6_11_21/Duration_Ex1.java
@@ -0,0 +1,13 @@
+package Date_Time_6_11_21;
+
+import java.time.Duration;
+import java.time.LocalTime;
+import java.time.temporal.ChronoUnit;
+
+public class Duration_Ex1 {
+public static void main(String[] args) {
+	Duration d=Duration.between(LocalTime.NOON,LocalTime.MAX);
+	System.out.println(d.get(ChronoUnit.SECONDS));
+}
+}
+//43199
