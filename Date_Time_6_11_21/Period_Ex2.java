2e411c3ab09f29424cb52ad8b80839d86ae016b5
manonmani <manonmani.pandukumar@gmail.com>
2021-11-06 17:13:53 +0530

    date and time

---
+++ Date_Time_6_11_21/Period_Ex2.java
@@ -0,0 +1,11 @@
+package Date_Time_6_11_21;
+
+import java.time.Period;
+
+public class Period_Ex2 {
+public static void main(String[] args) {
+	Period p=Period.of(2021, 2, 20);
+	System.out.println(p.toString());
+}
+}
+//P2021Y2M20D
