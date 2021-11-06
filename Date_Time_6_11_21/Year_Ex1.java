eeac7ed5035845efdb28f76eb698c9250328661f
manonmani <manonmani.pandukumar@gmail.com>
2021-11-06 17:31:31 +0530

    date and time

---
+++ Date_Time_6_11_21/Year_Ex1.java
@@ -0,0 +1,11 @@
+package Date_Time_6_11_21;
+
+import java.time.Year;
+
+public class Year_Ex1 {
+public static void main(String[] args) {
+	Year y=Year.now();
+	System.out.println(y);
+}
+}
+//2021
