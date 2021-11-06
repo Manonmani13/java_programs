1d57125b6ca95c4d88496594d2ec0c5919b59fbc
manonmani <manonmani.pandukumar@gmail.com>
2021-11-06 21:42:51 +0530

    date and time

---
+++ Date_time_6_11_21/AvaCal_Ex3.java
@@ -0,0 +1,11 @@
+package Date_time_6_11_21;
+
+import java.util.Calendar;
+
+public class AvaCal_Ex3 {
+	public static void main(String[] args) throws InterruptedException {  
+        // here we are directly printing the set which is  showing all available calendar type  
+    System.out.println(Calendar.getAvailableCalendarTypes());  
+    }    
+}
+//[gregory, buddhist, japanese]
