48455838cd96eddbffb8cf1a54f349cccb977f4c
manonmani <manonmani.pandukumar@gmail.com>
2021-11-06 16:42:30 +0530

    date and time

---
+++ Date_Time_6_11_21/Clone_Ex1.java
@@ -0,0 +1,11 @@
+package Date_Time_6_11_21;
+
+import java.util.Date;
+
+public class Clone_Ex1 {
+public static void main(String[] args) {
+	Date d=new Date(2018,9,21);
+	System.out.println(d.clone());
+}
+}
+//Mon Oct 21 00:00:00 IST 3918
