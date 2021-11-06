74a49b82ba16fd9b49093ec434595488cddcc57d
manonmani <manonmani.pandukumar@gmail.com>
2021-11-06 17:00:41 +0530

    date and time

---
+++ Date_Time_6_11_21/Equal_Ex2.java
@@ -0,0 +1,13 @@
+package Date_Time_6_11_21;
+
+import java.sql.Date;
+
+public class Equal_Ex2 {
+	
+	public static void main(String[] args) {
+		Date d=new Date(2018,9,21);
+		Date d1=new Date(2018,9,21);
+		System.out.println(d.equals(d1));
+	}
+}
+//true
