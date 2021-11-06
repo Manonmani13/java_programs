66fcf461ab7e2a13b38631f55c6cd22d2b6861db
manonmani <manonmani.pandukumar@gmail.com>
2021-11-06 17:25:07 +0530

    date and time

---
+++ Date_Time_6_11_21/ToInstant_Ex1.java
@@ -0,0 +1,12 @@
+package Date_Time_6_11_21;
+
+import java.util.Date;
+
+public class ToInstant_Ex1 {
+	public static void main(String[] args) {
+		Date d=new Date(1999,6,11);
+		
+		System.out.println(d.toInstant());
+	}
+}
+//3899-07-10T18:30:00Z
