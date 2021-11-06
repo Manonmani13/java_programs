ba37f384035f4973e8ec0e62e22d7b24eb3eb246
manonmani <manonmani.pandukumar@gmail.com>
2021-11-06 17:02:03 +0530

    date and time

---
+++ Date_Time_6_11_21/GetTime_Ex1.java
@@ -0,0 +1,13 @@
+package Date_Time_6_11_21;
+
+import java.util.Date;
+
+public class GetTime_Ex1 {
+	
+		public static void main(String[] args) {
+			Date d=new Date(1999,6,11);
+			
+			System.out.println(d.getTime());
+		}	
+	}
+	//60889861800000
