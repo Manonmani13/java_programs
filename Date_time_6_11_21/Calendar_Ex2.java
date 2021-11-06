2e2cb42bea94731deed75e5d9fe41368832f405e
manonmani <manonmani.pandukumar@gmail.com>
2021-11-06 21:47:12 +0530

    date and time

---
+++ Date_time_6_11_21/Calendar_Ex2.java
@@ -0,0 +1,11 @@
+package Date_time_6_11_21;
+
+import java.util.Calendar;
+
+public class Calendar_Ex2 {
+	  public static void main(String[] args) {  
+		   Calendar calendar = Calendar.getInstance();  
+		   System.out.println("At present Calendar's Year: " + calendar.get(Calendar.YEAR));  
+		   System.out.println("At present Calendar's Day: " + calendar.get(Calendar.DATE));  
+		   } 
+}
