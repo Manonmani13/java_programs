b401a10ae95c33e1adf922d1b3b2f4a7bc4de095
manonmani <manonmani.pandukumar@gmail.com>
2021-11-07 01:17:10 +0530

    date and time

---
+++ Date_Time_06_11_21/WIY_Ex2.java
@@ -0,0 +1,25 @@
+package Date_Time_06_11_21;
+import java.util.*;
+public class WIY_Ex2 {
+	public static void main(String args[])  
+    {    
+       Calendar mycal = Calendar.getInstance();  
+    mycal.setFirstDayOfWeek(Calendar.SUNDAY);  
+    mycal.setMinimalDaysInFirstWeek(7);  
+    mycal.setTimeInMillis(100);  
+    int wk = mycal.get(Calendar.WEEK_OF_YEAR);  
+    System.out.println("Week : " +wk);  
+    int wY = mycal.getWeekYear();  
+    System.out.println("Weekayear  : " +wY);  
+    int y = mycal.get(Calendar.YEAR);  
+    System.out.println("Year  : " +y);  
+        int w = mycal.getWeeksInWeekYear();  
+        System.out.println("Week : " +w);  
+    }  
+}
+/*
+Week : 52
+Weekayear  : 1969
+Year  : 1970
+Week : 52
+*/
