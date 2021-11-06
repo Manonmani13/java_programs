377e82bc3334d44b0038cfb1dcf31996c4195351
manonmani <manonmani.pandukumar@gmail.com>
2021-11-06 17:25:44 +0530

    date and time

---
+++ Date_Time_6_11_21/ToString_Ex1.java
@@ -0,0 +1,11 @@
+package Date_Time_6_11_21;
+
+import java.util.Date;
+
+public class ToString_Ex1 {
+public static void main(String[] args) {
+	Date d=new Date();
+	System.out.println(d.toString());
+}
+}
+//Sat Nov 06 14:55:38 IST 2021
