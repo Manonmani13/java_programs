3a57f3163797e099d1446c3b4e69b937586fdb92
manonmani <manonmani.pandukumar@gmail.com>
2021-11-07 01:12:25 +0530

    date and time

---
+++ Date_Time_06_11_21/TimeZone_Ex1.java
@@ -0,0 +1,18 @@
+package Date_Time_06_11_21;
+import java.util.*;
+public class TimeZone_Ex1 {
+	 public static void main(String[] args) {  
+	        Calendar mycal = Calendar.getInstance();  
+	                //getting the display name of TimeZone therwise it will return in object form  
+	        System.out.println(" TimeZone:"  
+	                + mycal.getTimeZone().getDisplayName());  
+	                // set the time zone of mycal to GMT        mycal.setTimeZone(TimeZone.getTimeZone("GMT"));  
+	        System.out.println("Time Zone after changing to GMT:"  
+	                + mycal.getTimeZone().getDisplayName());      
+	    }  
+}
+/*
+ TimeZone:India Standard Time
+Time Zone after changing to GMT:India Standard Time
+
+*/
