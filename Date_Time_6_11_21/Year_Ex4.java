e8fc22b73f1c4be71284b2e9492b9638f1a6298d
manonmani <manonmani.pandukumar@gmail.com>
2021-11-06 17:33:10 +0530

    date and time

---
+++ Date_Time_6_11_21/Year_Ex4.java
@@ -0,0 +1,11 @@
+package Date_Time_6_11_21;
+
+import java.time.Year;
+
+public class Year_Ex4 {
+public static void main(String[] args) {
+	Year y=Year.of(2016);
+	System.out.println(y.isLeap());
+}
+}
+//false
