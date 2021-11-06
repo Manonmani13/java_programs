b4cc1fbc5bfdbdcad70d408479d451bcfb572574
manonmani <manonmani.pandukumar@gmail.com>
2021-11-06 21:47:45 +0530

    date and time

---
+++ Date_time_6_11_21/Calender_Ex3.java
@@ -0,0 +1,11 @@
+package Date_time_6_11_21;
+
+import java.util.Calendar;
+
+public class Calender_Ex3 {
+	  public static void main(String[] args) {  
+		   Calendar calendar = Calendar.getInstance();  
+		   System.out.print("At present Date And Time Is: " + calendar.getTime());  
+		   }  
+}
+//At present Date And Time Is: Sat Nov 06 18:08:58 IST 2021
