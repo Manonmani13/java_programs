880609d0c30194d4cb1fb933c1911919aef8edef
manonmani <manonmani.pandukumar@gmail.com>
2021-11-07 22:38:36 +0530

    random class

---
+++ Random_7_11_21/NextByte_Ex2.java
@@ -0,0 +1,11 @@
+package Random_7_11_21;
+
+import java.util.Random;
+
+public class NextByte_Ex2 {
+	public static void main(String[] args) {
+	Random random = new Random();  
+	byte[] bytes = new byte[10];  
+	//System.out.print(random.nextBytes(bytes));  
+	    }  
+}
