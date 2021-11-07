204ba563d22cea96455b0cf3e0913c9cf5918f19
manonmani <manonmani.pandukumar@gmail.com>
2021-11-07 22:36:28 +0530

    random class

---
+++ Random_7_11_21/Next_Ex1.java
@@ -0,0 +1,11 @@
+package Random_7_11_21;
+
+import java.util.Random;
+
+public class Next_Ex1 extends Random {
+	public static void main(String[] args) {  
+        Next_Ex1 random = new Next_Ex1();  
+System.out.println("Next value returns = "+random.next(0b1001));  
+    }  
+}
+//Next value returns = 426
