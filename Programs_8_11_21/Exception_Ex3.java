5c2006712ccdb11e27ffef1445af6429a8d0d438
manonmani <manonmani.pandukumar@gmail.com>
2021-11-08 22:58:49 +0530

    exeption

---
+++ Programs_8_11_21/Exception_Ex3.java
@@ -0,0 +1,13 @@
+package Programs_8_11_21;
+
+public class Exception_Ex3 {
+	public static void main (String[] args) {
+        System.out.println(5/0);
+        System.out.println("End of program!");
+	}
+}
+/*
+Exception in thread "main" java.lang.ArithmeticException: / by zero
+	at Programs_8_11_21.Exception_Ex3.main(Exception_Ex3.java:5)
+
+*/
