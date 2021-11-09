acd1871a78785c04bbcb74dcefa7106a3e8e48d0
manonmani <manonmani.pandukumar@gmail.com>
2021-11-09 18:37:57 +0530

    javatpoint program

---
+++ Programs/Mainch8.java
@@ -0,0 +1,8 @@
+package Programs;
+
+public class Mainch8 {
+	 final strictfp public static  void main(String[] args) {
+			System.out.println("hi with final strictfp");
+		}
+}
+//hi with final strictfp
