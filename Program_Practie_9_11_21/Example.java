7e5fbcbf905ebc8b9fd1ff8fdaf005f9954dc272
manonmani <manonmani.pandukumar@gmail.com>
2021-11-09 23:02:06 +0530

    programs

---
+++ Program_Practie_9_11_21/Example.java
@@ -0,0 +1,27 @@
+package Program_Practie_9_11_21;
+
+public class Example {
+public static void main(String[] args) {
+		
+		int i=2;
+		int j=4;
+		while(i<=30)
+		{
+			System.out.println(i);
+			i=i+j;
+			j=j+2;
+			
+		}
+		
+
+
+	}
+}
+/*
+2
+6
+12
+20
+30
+
+*/
