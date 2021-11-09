cff5a5a19d18df4c896ce2e92a790e2e2dede3b3
manonmani <manonmani.pandukumar@gmail.com>
2021-11-09 18:36:29 +0530

    javatpoint program

---
+++ Programs/MainCh4.java
@@ -0,0 +1,13 @@
+package Programs;
+
+public class MainCh4 {
+public static int main(String[] args,int k) {
+	System.out.println("void changed as int");
+	return 10;
+}
+public static void main(String[] args) {
+	System.out.println("welcome ");
+}
+}
+// for this also i did not get output
+//welcome 
