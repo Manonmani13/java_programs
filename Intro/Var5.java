669af5ca6a31d6e95ca456613f45f53ba46f5e2b
manonmani <manonmani.pandukumar@gmail.com>
2021-11-09 19:16:57 +0530

    w3 program

---
+++ Intro/Var5.java
@@ -0,0 +1,10 @@
+package Intro;
+
+public class Var5 {
+public static void main(String[] args) {
+	final int myNum = 15;
+	//myNum = 20;we cannot modify final value
+	System.out.println(myNum);
+}
+}
+//15
