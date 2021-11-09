13f199403368c9921829183fbb791e9ef6e343d0
manonmani <manonmani.pandukumar@gmail.com>
2021-11-09 19:19:00 +0530

    w3 program

---
+++ Intro/Var11.java
@@ -0,0 +1,13 @@
+package Intro;
+
+public class Var11 {
+public static void main(String[] args) {
+	// Good
+	int minutesPerHour = 60;
+
+	// OK, but not so easy to understand what m actually is
+	int m = 60;
+	System.out.println(m);
+}
+}
+//60
