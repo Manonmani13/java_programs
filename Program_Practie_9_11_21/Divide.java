a62570c7c8c7f51bea60eb8281fa16e86e10b809
manonmani <manonmani.pandukumar@gmail.com>
2021-11-09 23:01:44 +0530

    programs

---
+++ Program_Practie_9_11_21/Divide.java
@@ -0,0 +1,18 @@
+package Program_Practie_9_11_21;
+
+public class Divide {
+	public static void main(String[] args) {
+		int num=16;
+		  
+		  int i=1;
+		  for(;i<=num;i++)
+		  {
+		  	if(num%i==0)
+		    {
+		    System.out.print(i+" ");
+		    }
+		  }
+
+	}
+}
+//1 2 4 8 16 
