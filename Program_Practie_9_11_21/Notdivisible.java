1d8a1999e2723d225f4feb250714d9544e97e2c0
manonmani <manonmani.pandukumar@gmail.com>
2021-11-09 23:02:40 +0530

    programs

---
+++ Program_Practie_9_11_21/Notdivisible.java
@@ -0,0 +1,26 @@
+package Program_Practie_9_11_21;
+
+public class Notdivisible {
+	public static void main(String[] args) {
+		 int num=20;
+
+		  for(int i=1;i<=num;i++)
+		  {
+		  	if(i%2!=0 && i%3!=0 && i%5!=0)
+		    {
+		    System.out.println(i);
+		    }
+		    
+		  }
+
+	}
+}
+/*
+1
+7
+11
+13
+17
+19
+
+*/
