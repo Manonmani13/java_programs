c6229d716f5af6840f9c9044e0be974bc3cbc8a0
manonmani <manonmani.pandukumar@gmail.com>
2021-11-09 19:18:11 +0530

    w3 program

---
+++ Intro/Var8.java
@@ -0,0 +1,11 @@
+package Intro;
+
+public class Var8 {
+public static void main(String[] args) {
+	String firstName = "Manonmani ";
+	String lastName = "Pandukumar";
+	String fullName = firstName + lastName;
+	System.out.println(fullName);
+}
+}
+//Manonmani Pandukumar
