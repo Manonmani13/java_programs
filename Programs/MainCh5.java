c7fefc74a49bdec309ac430cf062610027bcd70c
manonmani <manonmani.pandukumar@gmail.com>
2021-11-09 18:36:48 +0530

    javatpoint program

---
+++ Programs/MainCh5.java
@@ -0,0 +1,10 @@
+package Programs;
+
+public class MainCh5 {
+public static void main(String []args) {
+	System.out.println("String[] args");//String[] args
+	System.out.println("String args[]");//String args[]
+	System.out.println("String []args");//String []args
+}
+}
+//without Array brace it doesnt work
