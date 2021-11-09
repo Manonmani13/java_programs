83d3047f117e58768a4a146aa263a0281b60388d
manonmani <manonmani.pandukumar@gmail.com>
2021-11-09 23:10:57 +0530

    programs

---
+++ Program_Practie_9_11_21/SumOfEven.java
@@ -0,0 +1,15 @@
+package Program_Practie_9_11_21;
+
+public class SumOfEven {
+	public static void main(String[] args) {
+		int n=5;
+		int sum=0;
+		int i=0;
+		   for(;i<=n;i+=2)//0<=5 2<=5 4<=5
+		   	{
+			   sum=sum+i;//0+0=0 2,6
+		   	}  
+		   	System.out.println(sum);
+	}
+}
+//6
