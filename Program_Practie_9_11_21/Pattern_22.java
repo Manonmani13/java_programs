5e3bf6ceff78b45f262af1f673e7a4dce22764ef
manonmani <manonmani.pandukumar@gmail.com>
2021-11-09 23:05:24 +0530

    programs

---
+++ Program_Practie_9_11_21/Pattern_22.java
@@ -0,0 +1,22 @@
+package Program_Practie_9_11_21;
+
+public class Pattern_22 {
+public static void main(String[] args) {
+	for(int i=0;i<5;i++)
+	{
+		for(int j=0;j<5;j++)
+		{
+			System.out.print(i%2+" ");
+		}
+		System.out.println();
+	}
+}
+}
+/*
+0 0 0 0 0 
+1 1 1 1 1 
+0 0 0 0 0 
+1 1 1 1 1 
+0 0 0 0 0 
+
+*/
