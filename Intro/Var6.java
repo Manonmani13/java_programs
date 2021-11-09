c487bc07d73d3389303a9ed33d35d53514ad5acc
manonmani <manonmani.pandukumar@gmail.com>
2021-11-09 19:17:32 +0530

    w3 program

---
+++ Intro/Var6.java
@@ -0,0 +1,21 @@
+package Intro;
+
+public class Var6 {
+public static void main(String[] args) {
+	int myNum = 5;
+	float myFloatNum = 5.99f;
+	char myLetter = 'D';
+	boolean myBool = true;
+	String myText = "Hello";
+	System.out.println(myNum+"\n"+myFloatNum+"\n"+myLetter);
+	System.out.println(myBool+"\n"+myText);
+}
+}
+/*
+5
+5.99
+D
+true
+Hello
+
+*/
