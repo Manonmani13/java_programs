81fd6cea5e6fd3e4e7b5bf4cb9d1f8ff93638872
manonmani <manonmani.pandukumar@gmail.com>
2021-11-09 23:07:11 +0530

    programs

---
+++ Program_Practie_9_11_21/Pattern_27.java
@@ -0,0 +1,23 @@
+package Program_Practie_9_11_21;
+
+public class Pattern_27 {
+public static void main(String[] args) {
+	for(int i=0;i<5;i++)
+	{
+		for(int j=0;j<5;j++)
+		{
+			System.out.print((char)(j+65)+" ");
+		}
+		System.out.println();
+	}
+}
+}
+/*
+A B C D E 
+A B C D E 
+A B C D E 
+A B C D E 
+A B C D E 
+A B C D E 
+
+*/
