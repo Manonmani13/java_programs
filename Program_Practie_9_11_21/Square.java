5605742e66b243565e6fe9c15f269ab8770edb7e
manonmani <manonmani.pandukumar@gmail.com>
2021-11-09 23:10:39 +0530

    programs

---
+++ Program_Practie_9_11_21/Square.java
@@ -0,0 +1,28 @@
+package Program_Practie_9_11_21;
+
+public class Square {
+	public static void main(String[] args) {
+		int num=9;
+		  int sqrt;
+		  
+		  for(int i=1;i<=num;i++)
+		  {
+		  	sqrt=i*i;
+		    System.out.println("sqrt of"+" "+i+"="+sqrt);
+		    
+		  }
+
+	}
+}
+/*
+sqrt of 1=1
+sqrt of 2=4
+sqrt of 3=9
+sqrt of 4=16
+sqrt of 5=25
+sqrt of 6=36
+sqrt of 7=49
+sqrt of 8=64
+sqrt of 9=81
+
+*/
