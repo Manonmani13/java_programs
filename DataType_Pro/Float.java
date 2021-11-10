4ce770b27b3984389a9e0497ae565342564bb8d6
manonmani <manonmani.pandukumar@gmail.com>
2021-11-10 21:48:52 +0530

    w3 schools

---
+++ DataType_Pro/Float.java
@@ -0,0 +1,15 @@
+package DataType_Pro;
+
+public class Float {
+public static void main(String[] args) {
+	float f1 = 35e3f;
+	double d1 = 12E4d;
+	System.out.println(f1);
+	System.out.println(d1);
+}
+}
+/*
+35000.0
+120000.0
+
+*/
