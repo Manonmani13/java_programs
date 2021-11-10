f3bc4d9032982462de657dde3fafc2f0eb27fafe
manonmani <manonmani.pandukumar@gmail.com>
2021-11-10 22:18:04 +0530

    Programs

---
+++ BasicPrograms_10_11_21/Throw3.java
@@ -0,0 +1,25 @@
+package BasicPrograms_10_11_21;
+
+public class Throw3 {
+	static void method() throws ArithmeticException  
+    {  
+        System.out.println("Inside the method()");  
+        throw new ArithmeticException("throwing ArithmeticException");  
+    }  
+	public static void main(String args[])  
+    {  
+        try  
+        {  
+            method();  
+        }  
+        catch(ArithmeticException e)  
+        {  
+            System.out.println("caught in main() method");  
+        }  
+    }  
+}
+/*
+Inside the method()
+caught in main() method
+
+*/
