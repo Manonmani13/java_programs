9a7c4f5f4e7b52fa0cb49e1bfe2e5cb8439cb955
manonmani <manonmani.pandukumar@gmail.com>
2021-11-10 22:18:23 +0530

    Programs

---
+++ BasicPrograms_10_11_21/Throws_Ex1.java
@@ -0,0 +1,22 @@
+package BasicPrograms_10_11_21;
+
+import java.io.IOException;
+
+public class Throws_Ex1 {
+	void m()throws IOException{  
+	    throw new IOException("device error");//checked exception  
+	  }  
+	  void n()throws IOException{  
+	    m();  
+	  }  
+	  void p(){  
+	   try{  
+	    n();  
+	   }catch(Exception e){System.out.println("exception handled");}  
+	  }  
+	  public static void main(String args[]){  
+	   Throws_Ex1 obj=new Throws_Ex1();  
+	   obj.p();  
+	   System.out.println("normal flow...");  
+	  }  
+}
