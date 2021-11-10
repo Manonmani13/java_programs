ef509e6376c78d6337670c4d24ccfa2c4207101a
manonmani <manonmani.pandukumar@gmail.com>
2021-11-10 21:52:02 +0530

    w3 schools

---
+++ DataType_Pro/Typecasting1.java
@@ -0,0 +1,11 @@
+package DataType_Pro;
+
+public class Typecasting1 {
+	public static void main(String[] args) {
+	    int myInt = 9;
+	    double myDouble = myInt; // Automatic casting: int to double
+
+	    System.out.println(myInt);      // Outputs 9
+	    System.out.println(myDouble);   // Outputs 9.0
+	  }
+}
