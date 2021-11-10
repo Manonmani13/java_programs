a301cce929fc2f452078b8b10e5512a119364c18
manonmani <manonmani.pandukumar@gmail.com>
2021-11-10 21:54:35 +0530

    Exception

---
+++ BasicPrograms_10_11_21/Arith_Ex1.java
@@ -0,0 +1,13 @@
+package BasicPrograms_10_11_21;
+
+public class Arith_Ex1 {
+public static void main(String[] args) {
+	int no=10,no2=0;
+	System.out.println(no/no2);
+}
+}
+/*
+Exception in thread "main" java.lang.ArithmeticException: / by zero
+	at BasicPrograms_10_11_21.Arith_Ex1.main(Arith_Ex1.java:6)
+
+*/
