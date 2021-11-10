ca4b66121dbfc2bf7d3318145813c9e5ff4da6a9
manonmani <manonmani.pandukumar@gmail.com>
2021-11-10 21:49:34 +0530

    w3 schools

---
+++ DataType_Pro/Incr.java
@@ -0,0 +1,12 @@
+package DataType_Pro;
+
+public class Incr {
+public static void main(String[] args) {
+	 int x = 5;
+	    ++x;
+	    System.out.println(x);
+	    int x1 = 5;
+	    --x1;
+	    System.out.println(x1);//4
+}//6
+}
