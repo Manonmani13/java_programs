bde4a88aed0101ea462ccdff5e60b16da60400e3
manonmani <manonmani.pandukumar@gmail.com>
2021-11-10 22:15:42 +0530

    Programs

---
+++ BasicPrograms_10_11_21/Pattern_34.java
@@ -0,0 +1,21 @@
+package BasicPrograms_10_11_21;
+
+public class Pattern_34 {
+public static void main(String[] args) {
+	for(int i=1;i<=5;i++)
+	{
+		for(int j=1;j<=i;j++)
+		{
+			System.out.print("*");
+		}
+		System.out.println();
+	}
+}
+}
+/*
+*
+**
+***
+****
+*****
+*/
