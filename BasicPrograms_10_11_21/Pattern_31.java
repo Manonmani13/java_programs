5502610d92ab687dcc9f4b13df9a726e8529eac5
manonmani <manonmani.pandukumar@gmail.com>
2021-11-10 22:14:26 +0530

    Programs

---
+++ BasicPrograms_10_11_21/Pattern_31.java
@@ -0,0 +1,21 @@
+package BasicPrograms_10_11_21;
+
+public class Pattern_31 {
+public static void main(String[] args) {
+	for(int i=0;i<5;i++)
+	{
+		for(int j=0;j<5;j++)
+		{
+			System.out.print((char)(i+65)+" ");//0+1
+		}
+		System.out.println();
+	}
+}
+}
+/*
+A B C D E 
+B C D E F 
+C D E F G 
+D E F G H 
+E F G H I 
+*/
