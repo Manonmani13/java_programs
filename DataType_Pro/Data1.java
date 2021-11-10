e7d22c29e14323ab59b6c8e1754ea7e33dffcd6e
manonmani <manonmani.pandukumar@gmail.com>
2021-11-10 21:47:28 +0530

    w3 schools

---
+++ DataType_Pro/Data1.java
@@ -0,0 +1,13 @@
+package DataType_Pro;
+
+public class Data1 {
+public static void main(String[] args) {
+	int myNum = 5;               
+	float myFloatNum = 5.99f;    
+	char myLetter = 'D';         
+	boolean myBool = true;       
+	String myText = "Hello";
+	System.out.println(myNum+" "+myFloatNum+" "+myLetter+" "+myBool+" "+myText);
+}
+}
+//5 5.99 D true Hello
