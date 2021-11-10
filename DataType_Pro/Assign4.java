080c1175d236a18dcad26eb380137c07d9a0f50a
manonmani <manonmani.pandukumar@gmail.com>
2021-11-10 21:45:32 +0530

    w3 schools

---
+++ DataType_Pro/Assign4.java
@@ -0,0 +1,21 @@
+package DataType_Pro;
+
+public class Assign4 {
+public static void main(String[] args) {
+	  int x = 5;
+	    x |= 3;
+	    System.out.println(x);
+	    int x1 = 5;
+	    x1 ^= 3;
+	    System.out.println(x1);
+	    int x2 = 5;
+	    x2 >>= 3;
+	    System.out.println(x2);
+}
+}
+/*
+7
+6
+0
+
+*/
