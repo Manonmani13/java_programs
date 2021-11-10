1dfc73cad87a15f1214d54e2cce0c3823f3f9d55
manonmani <manonmani.pandukumar@gmail.com>
2021-11-10 21:44:52 +0530

    w3 schools

---
+++ DataType_Pro/Assign2.java
@@ -0,0 +1,12 @@
+package DataType_Pro;
+
+public class Assign2 {
+public static void main(String[] args) {
+	  int x = 5;
+	    x -= 3;
+	    System.out.println(x);//2
+	    int y = 5;
+	    y *= 3;
+	    System.out.println(y);//15
+}
+}
