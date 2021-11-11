f1d5c8e92bdc53f99a40884711b099c4759ef6d3
manonmani <manonmani.pandukumar@gmail.com>
2021-11-12 00:03:30 +0530

    File

---
+++ File/FileCreation.java
@@ -0,0 +1,10 @@
+package File;
+
+import java.io.File;
+
+public class FileCreation {
+public static void main(String[] args) {
+	File f=new File("/home/manon/Documents/File");
+	f.mkdir();//floder creation
+}
+}
