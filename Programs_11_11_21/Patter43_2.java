7452057afe3f297a9b65af519b7e09c5fbeec29d
manonmani <manonmani.pandukumar@gmail.com>
2021-11-12 00:09:11 +0530

    Pattern

---
+++ Programs_11_11_21/Patter43_2.java
@@ -0,0 +1,25 @@
+package Programs_11_11_21;
+
+public class Patter43_2 {
+public static void main(String[] args) {
+		
+		for(int i=1;i<=5;i++)
+		{
+		int k=1;
+			for(int j=0;j<i;j++)
+			{
+				System.out.print(i+j+2+" ");
+				
+			}
+			System.out.println();
+		}
+	}
+}
+/*3 
+4 5 
+5 6 7 
+6 7 8 9 
+7 8 9 10 11 
+
+
+*/
