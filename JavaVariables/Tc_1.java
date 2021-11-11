2525762661f4571d412e9f080a88230790f1b815
manonmani <manonmani.pandukumar@gmail.com>
2021-11-11 22:02:07 +0530

    variables

---
+++ JavaVariables/Tc_1.java
@@ -0,0 +1,11 @@
+package JavaVariables;
+
+public class Tc_1 {
+	public static void main(String[] args){  
+		float f=10.5f;  
+		//int a=f;//Compile time error  
+		int a=(int)f;  
+		System.out.println(f);  //10.5
+		System.out.println(a);  //10
+		}
+}
