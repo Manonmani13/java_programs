70498f7b041d021d707d109d60b2be850b5f0367
manonmani <manonmani.pandukumar@gmail.com>
2021-11-11 22:00:14 +0530

    variables

---
+++ JavaVariables/Ex1.java
@@ -0,0 +1,10 @@
+package JavaVariables;
+
+public class Ex1 {
+public static void main(String[] args) {
+	int data=20;
+	System.out.println(data);
+	//static int j=10;we annot give stati for local variable
+}
+}
+//20
