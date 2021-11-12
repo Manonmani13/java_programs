888c22b64bebbd4a1b654e087fe4f6edfc866ff3
manonmani <manonmani.pandukumar@gmail.com>
2021-11-12 21:33:44 +0530

    w3 schools

---
+++ TypeCasting/Ex1.java
@@ -0,0 +1,11 @@
+package TypeCasting;
+
+public class Ex1 {
+public static void main(String[] args) {
+	int myInt = 9;
+    double myDouble = myInt; // Automatic casting: int to double
+
+    System.out.println(myInt);      // Outputs 9
+    System.out.println(myDouble);
+}
+}
