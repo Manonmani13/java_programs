4191918e6cc3bbc0fa83be320667c2ab044d8d9c
manonmani <manonmani.pandukumar@gmail.com>
2021-11-12 22:07:56 +0530

    collection

---
+++ Collection_12_11_21/AL_Ex5.java
@@ -0,0 +1,35 @@
+package Collection_12_11_21;
+
+import java.util.ArrayList;
+
+public class AL_Ex5 {
+	public static void main(String[] args) {
+		ArrayList al=new ArrayList();
+		al.add('m');
+		al.add('a');
+		al.add('n');
+		al.add('o');
+		System.out.println(al);
+		al.add(true);
+		System.out.println(al);
+	System.out.println(al.contains(true));
+	System.out.println(al.indexOf('a'));
+	System.out.println(al.get(3));
+	al.remove(2);
+	System.out.println(al);
+	al.clear();
+	System.out.println(al);
+	System.out.println(al.isEmpty());
+	}
+}
+/*
+[m, a, n, o]
+[m, a, n, o, true]
+true
+1
+o
+false
+[m, a, o, true]
+[]
+true
+*/
