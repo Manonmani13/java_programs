5f86e315fb6c805f62d27472f83c045219b718f7
manonmani <manonmani.pandukumar@gmail.com>
2021-11-12 21:32:45 +0530

    w3 schools

---
+++ TypeCasting/Assign.java
@@ -0,0 +1,25 @@
+package TypeCasting;
+
+public class Assign {
+public static void main(String[] args) {
+	int x = 10;
+	System.out.println(x);
+	x += 5;
+	System.out.println(x);
+	   x -= 3;
+	System.out.println(x);
+	x *= 3;
+    System.out.println(x);
+    x /= 3;
+    System.out.println(x);
+    
+}
+}
+/*
+10
+15
+12
+36
+12
+
+*/
