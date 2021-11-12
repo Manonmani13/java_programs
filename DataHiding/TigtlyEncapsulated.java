c6bc6aedf9584d167ccbd7d43a98d4869c34166f
manonmani <manonmani.pandukumar@gmail.com>
2021-11-12 15:37:53 +0530

    durgasoft

---
+++ DataHiding/TigtlyEncapsulated.java
@@ -0,0 +1,16 @@
+package DataHiding;
+
+public class TigtlyEncapsulated {
+	private double balance;
+public static void main(String[] args) {
+	/*
+	 * each and every variable declare as private then it is tigtly encapsulated
+	 */
+	
+	
+}
+public double getBanlance()
+{
+	return  balance;
+}
+}
