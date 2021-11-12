3e2c230b3b5eb1636ff402d52f8cc2fb923d51a6
manonmani <manonmani.pandukumar@gmail.com>
2021-11-12 21:35:03 +0530

    w3 schools

---
+++ TypeCasting/Mul.java
@@ -0,0 +1,22 @@
+package TypeCasting;
+
+public class Mul {
+	public static void main(String[] args) {
+	    int x = 5;
+	    int y = 3;
+	    System.out.println(x * y);
+	    System.out.println(x / y);
+	    System.out.println(x % y);
+	    ++x;
+	    System.out.println(x);
+	    --y;
+	    System.out.println(y);
+	  }
+}
+/*
+15
+1
+2
+6
+2
+*/
