8266d2ae8e068ee30b3bfc385bc137c875382016
manonmani <manonmani.pandukumar@gmail.com>
2021-11-12 22:09:06 +0530

    collection

---
+++ Collection_12_11_21/ArrayList_E1.java
@@ -0,0 +1,15 @@
+package Collection_12_11_21;
+
+import java.util.ArrayList;
+
+public class ArrayList_E1 {
+public static void main(String[] args) {
+	ArrayList al=new ArrayList();
+	al.add('m');
+	al.add('a');
+	al.add('n');
+	al.add('o');
+	System.out.println(al);
+}
+}
+//[m, a, n, o]
