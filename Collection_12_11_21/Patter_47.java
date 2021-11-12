e0a3cab5eed28067c8ab7b5b5b8d69ee74b9b213
manonmani <manonmani.pandukumar@gmail.com>
2021-11-12 22:10:02 +0530

    collection

---
+++ Collection_12_11_21/Patter_47.java
@@ -0,0 +1,22 @@
+package Collection_12_11_21;
+
+public class Patter_47 {
+public static void main(String[] args) {
+	for(int i=1;i<=5;i++)
+	{
+		for(int j=1;j<=i;j++)
+		{
+			System.out.print(i*2+" ");
+		}
+		System.out.println();
+	}
+}
+}
+/*
+2 
+4 4 
+6 6 6 
+8 8 8 8 
+10 10 10 10 10 
+
+*/
