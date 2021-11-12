a163b416e38427bd571dc5d0d782b76fa078d7d7
manonmani <manonmani.pandukumar@gmail.com>
2021-11-12 21:33:23 +0530

    w3 schools

---
+++ TypeCasting/Com1.java
@@ -0,0 +1,24 @@
+package TypeCasting;
+
+public class Com1 {
+public static void main(String[] args) {
+	int x = 5;
+    int y = 3;
+    System.out.println(x == y);
+    System.out.println(x != y); 
+    System.out.println(x > y);
+    System.out.println(x < y); 
+    System.out.println(x >= y);
+    System.out.println(x <= y);
+}
+
+}
+/*
+false
+true
+true
+false
+true
+false
+
+*/
