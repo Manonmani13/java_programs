072f374d55f71c0a307af1f7f70d6f0abc0e90fc
manonmani <manonmani.pandukumar@gmail.com>
2021-11-12 21:36:18 +0530

    w3 schools

---
+++ TypeCasting/StringMethods.java
@@ -0,0 +1,10 @@
+package TypeCasting;
+
+public class StringMethods {
+public static void main(String[] args) {
+	String firstName = "John";
+	String lastName = "Doe";
+	System.out.println(firstName + " " + lastName);
+	System.out.println(firstName.concat(lastName));
+}
+}
