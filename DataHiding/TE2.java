7987cf7668bd484bd676d665c089e61c9dd78ea3
manonmani <manonmani.pandukumar@gmail.com>
2021-11-12 15:37:34 +0530

    durgasoft

---
+++ DataHiding/TE2.java
@@ -0,0 +1,17 @@
+package DataHiding;
+
+public class TE2 {
+int x=10;
+}
+/*class B extends TE2
+{
+private int y=20;	
+}
+class C extends B
+{
+	private int z=30;
+	
+}*/
+/*
+if the parent class is not tigtly encapsulated no child class is tigtly enca
+*/
