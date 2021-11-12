616859c7ecf6fc79bef05a4f92bf2e7bf8df316f
manonmani <manonmani.pandukumar@gmail.com>
2021-11-12 21:34:35 +0530

    w3 schools

---
+++ TypeCasting/Logical.java
@@ -0,0 +1,16 @@
+package TypeCasting;
+
+public class Logical {
+public static void main(String[] args) {
+	 int x = 5;
+	    System.out.println(x > 3 && x < 10);
+	    System.out.println(x > 3 || x < 4);
+	    System.out.println(!(x > 3 && x < 10));
+}
+}
+/*
+true
+true
+false
+
+*/
