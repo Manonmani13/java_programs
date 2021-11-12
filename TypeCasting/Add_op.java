d8079075dab8441766cd89ce8c8e621a902ddbd1
manonmani <manonmani.pandukumar@gmail.com>
2021-11-12 21:31:53 +0530

    w3 schools

---
+++ TypeCasting/Add_op.java
@@ -0,0 +1,13 @@
+package TypeCasting;
+
+public class Add_op {
+
+public static void main(String[] args) {
+    int x = 5;
+    int y = 3;
+    System.out.println(x + y);//8
+    int a = 5;
+    int b = 3;
+    System.out.println(a - b);//2
+  }
+}
