1310bb4bd7a7b0e4948c6512cba5985b013f7bb8
manonmani <manonmani.pandukumar@gmail.com>
2021-11-13 22:35:58 +0530

    String methods

---
+++ String_methods_10_11_21/NewKey.java
@@ -0,0 +1,9 @@
+package String_methods_10_11_21;
+
+public class NewKey {
+public static void main(String[] args) {
+	String s=new String("Welcome");//creates two objects and one reference variable  
+	System.out.println(s);
+}
+}
+//Welcome
