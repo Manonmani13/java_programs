7c199cb0f5cd9a4a4f9f4ebbaef6bc05ffcc301b
manonmani <manonmani.pandukumar@gmail.com>
2021-11-13 22:40:47 +0530

    String methods

---
+++ String_methods_10_11_21/StringBuffer_Ex1.java
@@ -0,0 +1,12 @@
+package String_methods_10_11_21;
+
+public class StringBuffer_Ex1 {
+	 public static void main(String[] args){  
+	        StringBuffer buffer=new StringBuffer("hello");  
+	        buffer.append("java");  
+	        System.out.println(buffer); 
+	        buffer.capacity();
+	        System.out.println(buffer);
+	    }  
+}
+//hellojava
