e989eb47342cc36ebc1a61a16c8ea938fb70e9de
manonmani <manonmani.pandukumar@gmail.com>
2021-11-13 22:27:18 +0530

    String methods

---
+++ String_methods_10_11_21/cancatnew.java
@@ -0,0 +1,14 @@
+package String_methods_10_11_21;
+
+public class cancatnew {
+public static void main(String[] args) {
+	String s="mano";
+	System.out.println(s);
+	s=s.concat("nmani");
+	System.out.println(s);
+}
+}
+/*
+mano
+manonmani
+*/
