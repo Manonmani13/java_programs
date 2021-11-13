b227bf1d1339eea384f569e91dfd53124b28af72
manonmani <manonmani.pandukumar@gmail.com>
2021-11-13 22:28:00 +0530

    String methods

---
+++ String_methods_10_11_21/CharAtEx1.java
@@ -0,0 +1,13 @@
+package String_methods_10_11_21;
+
+public class CharAtEx1 {
+public static void main(String[] args) {
+	String name="Manonmani";
+	System.out.println(name.charAt(0));
+	System.out.println(name.charAt(8));
+}
+}
+/*
+M
+i
+*/
