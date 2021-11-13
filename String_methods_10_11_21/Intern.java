219fa1b701ceff0b8fe364df5594a4b1e6a1cb44
manonmani <manonmani.pandukumar@gmail.com>
2021-11-13 22:34:51 +0530

    String methods

---
+++ String_methods_10_11_21/Intern.java
@@ -0,0 +1,11 @@
+package String_methods_10_11_21;
+
+public class Intern {
+	public static void main(String ar[])  
+	{  
+	String s=new String("Mano");    
+	String s2=s.intern();    
+	System.out.println(s2);   
+	}  
+}
+//Mano
