573f195a534d0e113b160dbf7486c400ee48b78d
manonmani <manonmani.pandukumar@gmail.com>
2021-11-13 22:34:24 +0530

    String methods

---
+++ String_methods_10_11_21/Insert.java
@@ -0,0 +1,10 @@
+package String_methods_10_11_21;
+
+public class Insert {
+public static void main(String[] args) {
+	StringBuffer s=new StringBuffer("Hello");
+	s.insert(0, "java");
+	System.out.println(s);
+}
+}
+//javaHello
