c17a9beb31502c31218dd11e2aae14a1a82c0ea5
manonmani <manonmani.pandukumar@gmail.com>
2021-11-13 22:41:18 +0530

    String methods

---
+++ String_methods_10_11_21/StringBuilder_Ex1.java
@@ -0,0 +1,10 @@
+package String_methods_10_11_21;
+
+public class StringBuilder_Ex1 {
+	public static void main(String[] args){  
+        StringBuilder builder=new StringBuilder("hello");  
+        builder.append("java");  
+        System.out.println(builder);  
+    }  
+}
+//hellojava
