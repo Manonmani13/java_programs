17c46527e112879fe200ece7f1ad91ef8804e1a5
manonmani <manonmani.pandukumar@gmail.com>
2021-11-13 22:38:37 +0530

    String methods

---
+++ String_methods_10_11_21/Reverse.java
@@ -0,0 +1,10 @@
+package String_methods_10_11_21;
+
+public class Reverse {
+public static void main(String[] args) {
+	StringBuffer b=new StringBuffer("madam");
+	b.reverse();
+	System.out.println(b);
+}
+}
+//madam
