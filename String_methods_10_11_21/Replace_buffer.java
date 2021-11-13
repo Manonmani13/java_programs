5e794f6f534ba5c777ae8c67c38e509d02f507ad
manonmani <manonmani.pandukumar@gmail.com>
2021-11-13 22:36:18 +0530

    String methods

---
+++ String_methods_10_11_21/Replace_buffer.java
@@ -0,0 +1,11 @@
+package String_methods_10_11_21;
+
+public class Replace_buffer {
+public static void main(String[] args) {
+	StringBuffer b=new StringBuffer("Hello");
+	b.replace(0, 3,"java");
+	System.out.println(b);
+
+}
+}
+//javalo
