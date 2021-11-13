7ff2deae69709e4af18ccc526dc176c8fb951b76
manonmani <manonmani.pandukumar@gmail.com>
2021-11-13 22:40:06 +0530

    String methods

---
+++ String_methods_10_11_21/Stribuinsert.java
@@ -0,0 +1,10 @@
+package String_methods_10_11_21;
+
+public class Stribuinsert {
+public static void main(String[] args) {
+	StringBuilder b=new StringBuilder("manonmani");
+	b.insert(0, "java");
+	System.out.println(b);
+}
+}
+//javamanonmani
