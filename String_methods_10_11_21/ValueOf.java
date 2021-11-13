0c7404b46de45f7e0d66bb427c0a0c2bb85ec2b9
manonmani <manonmani.pandukumar@gmail.com>
2021-11-13 22:45:17 +0530

    String methods

---
+++ String_methods_10_11_21/ValueOf.java
@@ -0,0 +1,38 @@
+package String_methods_10_11_21;
+
+public class ValueOf {
+public static void main(String[] args) {
+int i=10;
+byte b=20;
+short s=5;
+long l=30;
+float f=3.0f;
+double d=9.0;
+char c='a';
+boolean bl=true;
+String s1=String.valueOf(i);
+System.out.println(s1);
+String h=String.valueOf(b);
+System.out.println(h);
+String s2=String.valueOf(s);
+System.out.println(s2);
+String s3=String.valueOf(l);
+System.out.println(s3);
+String s4=String.valueOf(f);
+System.out.println(s4);
+String s5=String.valueOf(d);
+System.out.println(s5);
+String s6=String.valueOf(c);
+System.out.println(s6);
+}
+}
+/*
+10
+20
+5
+30
+3.0
+9.0
+a
+
+*/
