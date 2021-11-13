5dbde460c402d9152486c8b713feac606199c2fc
manonmani <manonmani.pandukumar@gmail.com>
2021-11-13 22:42:14 +0530

    String methods

---
+++ String_methods_10_11_21/StringFormat.java
@@ -0,0 +1,14 @@
+package String_methods_10_11_21;
+
+public class StringFormat {
+public static void main(String[] args) {
+	String na="mano";
+	String s="mani";
+	int i=10;
+	String ss=String.format("%s %s %d",s, na,i);
+	System.out.println(ss);
+
+
+}
+}
+//mani mano 10
