0becc555f142569de9c47804bc8f37beb1de8484
manonmani <manonmani.pandukumar@gmail.com>
2021-11-13 22:42:51 +0530

    String methods

---
+++ String_methods_10_11_21/StringLiterals.java
@@ -0,0 +1,15 @@
+package String_methods_10_11_21;
+
+public class StringLiterals {
+public static void main(String[] args) {
+	String s="mano";
+	System.out.println(s);
+	String s1="mano";
+	System.out.println("Already created  "+s1);
+}
+}
+/*
+mano
+Already created  mano
+
+*/
