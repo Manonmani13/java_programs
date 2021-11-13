a3c2e9a7d99b6159a11d7a601d08b8af77019080
manonmani <manonmani.pandukumar@gmail.com>
2021-11-13 22:32:26 +0530

    String methods

---
+++ String_methods_10_11_21/EnsureCapacity.java
@@ -0,0 +1,25 @@
+package String_methods_10_11_21;
+
+public class EnsureCapacity {
+public static void main(String[] args) {
+	StringBuffer sb=new StringBuffer();  
+	System.out.println(sb.capacity());//default 16  
+	sb.append("Hello");  
+	System.out.println(sb.capacity());//now 16  
+	sb.append("java is my favourite language");  
+	System.out.println(sb.capacity());//now (16*2)+2=34 i.e (oldcapacity*2)+2  
+	sb.ensureCapacity(10);//now no change  
+	System.out.println(sb.capacity());//now 34  
+	sb.ensureCapacity(50);//now (34*2)+2  
+	System.out.println(sb.capacity());//now 70  
+
+}
+}
+/*
+16
+16
+34
+34
+70
+
+*/
