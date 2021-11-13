f9893d9bfb69d63e56a77cb75bffe08622167aef
manonmani <manonmani.pandukumar@gmail.com>
2021-11-13 22:33:57 +0530

    String methods

---
+++ String_methods_10_11_21/Ex_St.java
@@ -0,0 +1,19 @@
+package String_methods_10_11_21;
+
+public class Ex_St {
+public static void main(String[] args) {
+	String v="mano";
+	char ch[]= {'k','i','r','t','h','u'};
+	String s3=new String("example");
+	String s=new String(ch);
+System.out.println(s);
+	System.out.println(v);
+	System.out.println(s3);
+}
+}
+/*
+kirthu
+mano
+example
+
+*/
