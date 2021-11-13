841438ffc8b4453b6542cad07f8f86a4d7fd98e1
manonmani <manonmani.pandukumar@gmail.com>
2021-11-13 22:41:49 +0530

    String methods

---
+++ String_methods_10_11_21/Stringchar.java
@@ -0,0 +1,18 @@
+package String_methods_10_11_21;
+
+public class Stringchar {
+public static void main(String[] args) {
+	char ch[]= {'m','a','n','o'};
+	String s=new String(ch);
+	System.out.println(s);
+	for(int i=0;i<ch.length;i++)
+	{
+		System.out.print(ch[i]+" ");
+	}
+	
+}
+}
+/*
+mano
+m a n o 
+*/
