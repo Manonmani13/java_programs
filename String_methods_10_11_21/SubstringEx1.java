abf2a4280fc9e2ed56b69829d0841c952b008662
manonmani <manonmani.pandukumar@gmail.com>
2021-11-13 22:44:30 +0530

    String methods

---
+++ String_methods_10_11_21/SubstringEx1.java
@@ -0,0 +1,11 @@
+package String_methods_10_11_21;
+
+public class SubstringEx1 {
+public static void main(String[] args) {
+	String s="Manonmani Pandukumar";    
+	 System.out.println("Original String: " + s);  
+	 System.out.println("Substring starting from index 6: " +s.substring(6));//  ani Pandukumar
+	 System.out.println("Substring starting from index 0 to 6: "+s.substring(0,6)); // Manonm
+
+}
+}
