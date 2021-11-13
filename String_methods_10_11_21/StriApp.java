43af16b5f52d4a4297cd70f4d01d06160721685a
manonmani <manonmani.pandukumar@gmail.com>
2021-11-13 22:39:20 +0530

    String methods

---
+++ String_methods_10_11_21/StriApp.java
@@ -0,0 +1,12 @@
+package String_methods_10_11_21;
+
+public class StriApp {
+public static void main(String[] args) {
+	String n=new String("happy");
+	String s1=new String("birthday");
+    String s = String.format("%s%s",n,s1); 
+    System.out.println(s);
+    
+}
+}
+//happybirthday
