dd03ffb740532b8b8d920b43df2ba9857721fd0e
manonmani <manonmani.pandukumar@gmail.com>
2021-11-13 22:38:59 +0530

    String methods

---
+++ String_methods_10_11_21/Stars_Ends.java
@@ -0,0 +1,15 @@
+package String_methods_10_11_21;
+
+public class Stars_Ends {
+public static void main(String[] args) {
+	String name="Manonmani";
+	System.out.println(name.startsWith("M"));
+	System.out.println(name.endsWith("i"));
+	System.out.println(name.startsWith("s"));
+}
+}
+/*
+true
+true
+false
+*/
