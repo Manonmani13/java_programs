c11682ef5c74ea5e696dd189a6a9d3264f540e4e
manonmani <manonmani.pandukumar@gmail.com>
2021-11-13 22:30:41 +0530

    String methods

---
+++ String_methods_10_11_21/Concat.java
@@ -0,0 +1,10 @@
+package String_methods_10_11_21;
+
+public class Concat {
+public static void main(String[] args) {
+	 String s="Sachin";  
+	   s.concat(" Tendulkar");//concat() method appends the string at the end  
+	   System.out.println(s);
+}
+}
+//Sachin
