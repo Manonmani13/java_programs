f8f3093d87158730acc9713e19aa6b07737071e2
manonmani <manonmani.pandukumar@gmail.com>
2021-11-13 22:43:18 +0530

    String methods

---
+++ String_methods_10_11_21/StriReverse.java
@@ -0,0 +1,10 @@
+package String_methods_10_11_21;
+
+public class StriReverse {
+public static void main(String[] args) {
+	StringBuilder s=new StringBuilder("Hello");
+	s.replace(1,3,"Java");
+	System.out.println(s);
+}
+}
+//HJavalo
