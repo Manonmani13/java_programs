7f11771c1a5f675fc4658fd31f5b60e90019ed02
manonmani <manonmani.pandukumar@gmail.com>
2021-11-13 22:36:41 +0530

    String methods

---
+++ String_methods_10_11_21/Replace.java
@@ -0,0 +1,10 @@
+package String_methods_10_11_21;
+
+public class Replace {
+public static void main(String[] args) {
+	String se="Java is oop language . Java is simple language.";
+	String rep=se.replace("Java", "Mava");
+	System.out.println(rep);
+}
+}
+//Mava is oop language . Mava is simple language.
