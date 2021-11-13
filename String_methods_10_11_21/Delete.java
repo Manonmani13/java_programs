d4bc1ea6787160ceb81b061fd397039c60ab0fdf
manonmani <manonmani.pandukumar@gmail.com>
2021-11-13 22:32:03 +0530

    String methods

---
+++ String_methods_10_11_21/Delete.java
@@ -0,0 +1,10 @@
+package String_methods_10_11_21;
+
+public class Delete {
+public static void main(String[] args) {
+	StringBuffer b=new StringBuffer("Hello");
+	b.delete(0, 2);
+	System.out.println(b);
+}
+}
+//llo
