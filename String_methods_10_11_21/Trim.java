fa608df50acb7fef32122491694dd936609c30dc
manonmani <manonmani.pandukumar@gmail.com>
2021-11-13 22:44:44 +0530

    String methods

---
+++ String_methods_10_11_21/Trim.java
@@ -0,0 +1,16 @@
+package String_methods_10_11_21;
+
+public class Trim {
+public static void main(String[] args) {
+	
+	String s="  Manonmani  Pandukumar  ";    
+	System.out.println(s);     
+	System.out.println(s.trim());    
+	
+}
+}
+/*
+  Manonmani  Pandukumar  
+Manonmani  Pandukumar
+
+*/
