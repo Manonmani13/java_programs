5240c3e51db8d3c6aee6f8909e3d7de630677675
manonmani <manonmani.pandukumar@gmail.com>
2021-11-13 22:45:00 +0530

    String methods

---
+++ String_methods_10_11_21/UpperCaseAndLowerCase.java
@@ -0,0 +1,16 @@
+package String_methods_10_11_21;
+
+public class UpperCaseAndLowerCase {
+public static void main(String[] args) {
+	String name="Mano";
+	System.out.println(name.toUpperCase());
+	System.out.println(name.toLowerCase());
+	System.out.println(name);
+}
+}
+/*
+MANO
+mano
+Mano
+
+*/
