963de4b1d93f38b113c3b52ac002c1b00ef3549b
manonmani <manonmani.pandukumar@gmail.com>
2021-11-13 22:29:55 +0530

    String methods

---
+++ String_methods_10_11_21/Concat_meth.java
@@ -0,0 +1,11 @@
+package String_methods_10_11_21;
+
+public class Concat_meth {
+public static void main(String[] args) {
+	   String s1="Manonmani ";  
+	   String s2="Pandukumar";  
+	   String s3=s1.concat(s2);  
+	   System.out.println(s3);//
+}
+}
+//Manonmani Pandukumar
