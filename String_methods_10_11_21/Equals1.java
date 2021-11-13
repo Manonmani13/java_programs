ad92ff63fbc316823683a0d6494bf8777571286e
manonmani <manonmani.pandukumar@gmail.com>
2021-11-13 22:33:14 +0530

    String methods

---
+++ String_methods_10_11_21/Equals1.java
@@ -0,0 +1,19 @@
+package String_methods_10_11_21;
+
+public class Equals1 {
+public static void main(String[] args) {
+	String name="mano";
+	String name1="mano";
+	String n=new String("mano");
+	System.out.println(name.equals(name1));
+	System.out.println(name.equals(n));
+	String nm="maha";
+	System.out.println(name.equals(nm));
+}
+}
+/*
+true
+true
+false
+
+*/
