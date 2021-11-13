b6dcd7faee11eb3b7c5be3246d6757b5da8c1f25
manonmani <manonmani.pandukumar@gmail.com>
2021-11-13 22:26:25 +0530

    String methods

---
+++ String_methods_10_11_21/Append_Ex1.java
@@ -0,0 +1,10 @@
+package String_methods_10_11_21;
+
+public class Append_Ex1 {
+public static void main(String[] args) {
+	StringBuffer b=new StringBuffer("Hi");
+	b.append(" java");
+	System.out.println(b);
+}
+}
+//Hi java
