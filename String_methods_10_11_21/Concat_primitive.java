632adf134502858681cd278e94e7f3b97a0db26d
manonmani <manonmani.pandukumar@gmail.com>
2021-11-13 22:30:20 +0530

    String methods

---
+++ String_methods_10_11_21/Concat_primitive.java
@@ -0,0 +1,16 @@
+package String_methods_10_11_21;
+
+public class Concat_primitive {
+public static void main(String[] args) {
+	String s=80+"manonmani"+40+30;
+	System.out.println(s);
+	String s1="kala";
+	System.out.println(s.equals(s1));
+	
+}
+public boolean equals(Object s1)
+{
+	return true;
+}
+}
+//80manonmani4030
