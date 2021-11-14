696b1f50b095456e9604f207498783c149752394
manonmani <manonmani.pandukumar@gmail.com>
2021-11-14 23:07:09 +0530

    String methods

---
+++ StringMethods/Compare5.java
@@ -0,0 +1,15 @@
+package StringMethods;
+
+public class Compare5 {
+	public static void main(String[] args)   
+	{  
+	   
+	String str = null;  
+	  
+	// null is invoking the compareTo method. Hence, the NullPointerException  
+	// will be raised  
+	int no =  str.compareTo("India is my country.");  
+	  
+	System.out.println(no);  
+	}  
+}
