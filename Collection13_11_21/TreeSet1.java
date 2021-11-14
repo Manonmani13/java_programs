46c6c47cce173e0a2ecd03e15f00f6f444a66010
manonmani <manonmani.pandukumar@gmail.com>
2021-11-14 22:58:06 +0530

    Set

---
+++ Collection13_11_21/TreeSet1.java
@@ -0,0 +1,21 @@
+package Collection13_11_21;
+
+import java.util.LinkedHashSet;
+import java.util.TreeSet;
+
+public class TreeSet1 {
+	public static void main(String[] args) {
+		TreeSet s=new TreeSet();
+		s.add(1190);
+		s.add(2);
+		s.add(800);
+		s.add(3082);
+		//s.add("abc");
+		System.out.println(s);
+		System.out.println(s.size());
+	}
+}
+/*
+[2, 800, 1190, 3082]
+4
+*/
