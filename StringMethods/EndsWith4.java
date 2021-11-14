6959119a9222dc740f35bdfe727be4c975cfdd8a
manonmani <manonmani.pandukumar@gmail.com>
2021-11-14 23:17:28 +0530

    String methods

---
+++ StringMethods/EndsWith4.java
@@ -0,0 +1,18 @@
+package StringMethods;
+
+public class EndsWith4 {
+public static void main(String[] args) {
+	String str = "Welcome to JavaTpoint!";  
+	  
+	// invoking the method endsWith with the parameter as null  
+	if(str.endsWith(null))  
+	{  
+	System.out.println("Inside the if block");  
+	}  
+	else  
+	{  
+	System.out.println("Inside the else block"); 
+	}
+
+}
+}
