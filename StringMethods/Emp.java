57b73a32f519309587298e53ed9c3b6e7405aaa9
manonmani <manonmani.pandukumar@gmail.com>
2021-11-14 23:14:38 +0530

    String methods

---
+++ StringMethods/Emp.java
@@ -0,0 +1,25 @@
+package StringMethods;
+
+public  final class Emp {
+	final String pancardNumber;    
+	public Emp(String pancardNumber)  
+	{    
+	this.pancardNumber=pancardNumber;    
+	}  
+	public String getPancardNumber(){    
+	return pancardNumber;    
+	}    
+	}    
+ class ImmutableDemo  
+	{  
+	public static void main(String ar[])  
+	{  
+	Emp e = new Emp("ABC123");  
+	String s1 = e.getPancardNumber();  
+	System.out.println("Pancard Number: " + s1);  
+	} 
+}
+/*
+
+
+ */
