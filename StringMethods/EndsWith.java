c588dd7e79f12aa2f115e620164f4818116691f1
manonmani <manonmani.pandukumar@gmail.com>
2021-11-14 23:15:23 +0530

    String methods

---
+++ StringMethods/EndsWith.java
@@ -0,0 +1,14 @@
+package StringMethods;
+
+public class EndsWith {
+	public static void main(String args[]){  
+		String s1="java by java";  
+		System.out.println(s1.endsWith("va"));  
+		System.out.println(s1.endsWith("p"));  
+		}
+}
+/*
+true
+false
+
+*/
