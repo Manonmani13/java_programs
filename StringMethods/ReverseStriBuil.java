5784d2bc57cd5c66711965ab330dd02a1d239b12
manonmani <manonmani.pandukumar@gmail.com>
2021-11-14 23:30:40 +0530

    String methods

---
+++ StringMethods/ReverseStriBuil.java
@@ -0,0 +1,10 @@
+package StringMethods;
+
+public class ReverseStriBuil {
+public static void main(String[] args) {
+	StringBuilder b=new StringBuilder("Hello");
+	b.reverse();
+	System.out.println(b);
+}
+}
+//olleH
