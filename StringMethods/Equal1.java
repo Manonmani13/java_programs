2223866b18f59663c1380cc9b6d0b289a68f7ca6
manonmani <manonmani.pandukumar@gmail.com>
2021-11-14 23:19:51 +0530

    String methods

---
+++ StringMethods/Equal1.java
@@ -0,0 +1,20 @@
+package StringMethods;
+
+public class Equal1 {
+public static void main(String[] args) {
+	String s="manonmani";
+	String s2="manonmani";
+	String s3="MANONMANI";
+	String s4="mano";
+	System.out.println(s.equals(s4));
+	System.out.println(s.equals(s2));
+	System.out.println(s.equals(s3));
+			
+}
+}
+/*
+false
+true
+false
+
+*/
