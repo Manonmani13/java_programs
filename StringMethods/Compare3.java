e54080f3558460ae72f5742ee7681ae4dbb0a86f
manonmani <manonmani.pandukumar@gmail.com>
2021-11-14 23:06:54 +0530

    String methods

---
+++ StringMethods/Compare3.java
@@ -0,0 +1,16 @@
+package StringMethods;
+
+public class Compare3 {
+	public static void main(String argvs[])  
+	{  
+	  
+	// input string in uppercase  
+	String st1 = new String("INDIA IS MY COUNTRY");  
+	  
+	// input string in lowercase  
+	String st2 = new String("india is my country");  
+	  
+	System.out.println(st1.compareTo(st2));  
+	}  
+}
+//-32
