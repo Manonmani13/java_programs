941c188c2cd7ccb0e8524e556cce52c5f6b95d6b
manonmani <manonmani.pandukumar@gmail.com>
2021-11-14 22:54:59 +0530

    Set

---
+++ Collection13_11_21/Array.java
@@ -0,0 +1,15 @@
+package Collection13_11_21;
+
+import java.util.ArrayList;
+
+public class Array {
+public static void main(String[] args) {
+	Array l=new Array();
+	Array s=new Array();
+	ArrayList al=new ArrayList();
+	al.add(l);
+	al.add(s);
+	System.out.println(al);
+}
+}
+//[Collection13_11_21.Array@6ff3c5b5, Collection13_11_21.Array@3764951d]
