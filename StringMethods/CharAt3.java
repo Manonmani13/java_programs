efc36ac7825810ef93a1d7c71d632ae0481d86c9
manonmani <manonmani.pandukumar@gmail.com>
2021-11-14 23:03:34 +0530

    String methods

---
+++ StringMethods/CharAt3.java
@@ -0,0 +1,15 @@
+package StringMethods;
+
+public class CharAt3 {
+public static void main(String[] args) {
+String s="hi i am manonmani";
+int st=s.length();
+System.out.println("Character at 0 index is: "+ s.charAt(0));
+System.out.println("Character at last index is: "+ s.charAt(st-1));      
+
+}
+}
+/*
+Character at 0 index is: h
+Character at last index is: i
+*/
