ddf3ffefff77e274a74aab8a343732aeb38894e3
manonmani <manonmani.pandukumar@gmail.com>
2021-11-14 22:57:08 +0530

    Set

---
+++ Collection13_11_21/Set1.java
@@ -0,0 +1,14 @@
+package Collection13_11_21;
+
+public class Set1 {
+public static void main(String[] args) {
+	String s1=new String("Santhiya");
+	String s2=new String("Santhiya");
+	System.out.println(s1.hashCode());
+	System.out.println(s2.hashCode());
+}
+}
+/*
+2072418941
+2072418941
+*/
