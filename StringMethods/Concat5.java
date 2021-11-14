1d8ef62366c7b24be5f7d46cd52251246085407e
manonmani <manonmani.pandukumar@gmail.com>
2021-11-14 23:10:06 +0530

    String methods

---
+++ StringMethods/Concat5.java
@@ -0,0 +1,17 @@
+package StringMethods;
+
+public class Concat5 {
+	public static void main(String argvs[])  
+	{  
+	String str = "Country";  
+	  
+	// we have added the string "India is my" before the String str;  
+	// Also, observe that a string literal can also invoke the concat() method  
+	String s = "India is my ".concat(str);  
+	  
+	// displaying the string  
+	System.out.println(s);  
+	  
+	}  
+}
+//India is my Country
