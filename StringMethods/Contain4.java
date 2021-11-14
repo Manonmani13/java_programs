76bb01567c172406e254f1656eadf3652b9bb676
manonmani <manonmani.pandukumar@gmail.com>
2021-11-14 23:11:00 +0530

    String methods

---
+++ StringMethods/Contain4.java
@@ -0,0 +1,19 @@
+package StringMethods;
+
+public class Contain4 {
+	public static void main(String argvs[])  
+	{  
+	String str = "Welcome to Java!";  
+	  
+	// comparing a string to null  
+	if(str.contains(null))  
+	{  
+	System.out.println("Inside the if block");  
+	}  
+	else  
+	{  
+	System.out.println("Inside the else block");  
+	}  
+	  
+	}  
+}
