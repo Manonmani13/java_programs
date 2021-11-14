9e92647506e18fb5cead82d79ef9561fc21dfcf2
manonmani <manonmani.pandukumar@gmail.com>
2021-11-14 23:31:06 +0530

    String methods

---
+++ StringMethods/StbuiCap.java
@@ -0,0 +1,18 @@
+package StringMethods;
+
+public class StbuiCap {
+
+public static void main(String args[]){    
+	StringBuilder sb=new StringBuilder();    
+	System.out.println(sb.capacity());//default 16    
+	sb.append("Hello");    
+	System.out.println(sb.capacity());//now 16    
+	sb.append("Java is my favourite language");    
+	System.out.println(sb.capacity());//now (16*2)+2=34 i.e (oldcapacity*2)+2    
+	} 
+}/*
+16
+16
+34
+
+*/
