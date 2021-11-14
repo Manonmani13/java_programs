b39ba5d1a74964094a6dd23f4166454f9ba6948b
manonmani <manonmani.pandukumar@gmail.com>
2021-11-14 22:57:30 +0530

    Set

---
+++ Collection13_11_21/Sethash.java
@@ -0,0 +1,15 @@
+package Collection13_11_21;
+
+public class Sethash {
+public static void main(String[] args) {
+	Sethash s=new Sethash();
+	Sethash s2=new Sethash();
+	System.out.println(s.hashCode());
+	System.out.println(s2.hashCode());
+}
+}
+/*
+1878246837
+929338653
+
+*/
