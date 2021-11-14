89b659053e3ab39e4fff9a2daf12cfde8904dfe6
manonmani <manonmani.pandukumar@gmail.com>
2021-11-14 23:02:12 +0530

    String methods

---
+++ StringMethods/Char6.java
@@ -0,0 +1,80 @@
+package StringMethods;
+
+import java.util.ArrayList;
+
+public class Char6 {
+	ArrayList<Character> al;  
+	  
+	// constructor for creating and   
+	// assigning values to the ArrayList al  
+	Char6()  
+	{  
+	al = new ArrayList<Character>();    
+	al.add('A'); al.add('E');  
+	al.add('a'); al.add('e');  
+	al.add('I'); al.add('O');  
+	al.add('i'); al.add('o');  
+	al.add('U'); al.add('u');  
+	}  
+	   
+	// a method that checks whether the character c is a vowel or not     
+	private boolean isVowel(char c)  
+	{  
+	for(int i = 0; i < al.size(); i++)  
+	{  
+	if(c == al.get(i))  
+	{  
+	return true;  
+	}  
+	}  
+	return false;  
+	}  
+	// a method that calculates vowels in the String s  
+	public int countVowels(String s)  
+	{  
+	int countVowel = 0; // store total number of vowels  
+	int size = s.length(); // size of string  
+	for(int j = 0; j < size; j++)  
+	{  
+	char c = s.charAt(j);  
+	if(isVowel(c))  
+	{  
+	// vowel found!  
+	// increase the count by 1  
+	countVowel = countVowel + 1;  
+	}  
+	}  
+	  
+	return countVowel;  
+	}  
+	      
+	// main method  
+	public static void main(String argvs[])   
+	{    
+	// creating an object of the class CharAtExample6  
+	Char6 obj = new Char6();  
+	  
+	String str = "Javatpoint is a great site for learning Java.";    
+	  
+	int noOfVowel = obj.countVowels(str);  
+	  
+	System.out.println("String: " + str);  
+	  
+	System.out.println("Total number of vowels in the string are: "+ noOfVowel + "\n");    
+	  
+	str = "One apple in a day keeps doctor away.";  
+	  
+	System.out.println("String: " + str);  
+	  
+	noOfVowel = obj.countVowels(str);  
+	  
+	System.out.println("Total number of vowels in the string are: "+ noOfVowel);   
+	}    
+}
+/*
+String: Javatpoint is a great site for learning Java.
+Total number of vowels in the string are: 16
+
+String: One apple in a day keeps doctor away.
+Total number of vowels in the string are: 13
+*/
