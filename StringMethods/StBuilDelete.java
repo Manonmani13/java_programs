e82bc0aa240400770910de14dff13af0107cd382
manonmani <manonmani.pandukumar@gmail.com>
2021-11-14 23:31:47 +0530

    String methods

---
+++ StringMethods/StBuilDelete.java
@@ -0,0 +1,10 @@
+package StringMethods;
+
+public class StBuilDelete {
+public static void main(String[] args) {
+	StringBuilder s=new StringBuilder("mano");
+	s.delete(1, 2);
+	System.out.println(s);
+}
+}
+//mno
