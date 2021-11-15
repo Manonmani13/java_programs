8db48ad835a22cfb7cf539e6a790404b04d0382c
manonmani <manonmani.pandukumar@gmail.com>
2021-11-15 17:58:23 +0530

    String methods

---
+++ StringMethos_2/Indexof6.java
@@ -0,0 +1,15 @@
+package StringMethos_2;
+
+public class Indexof6 {
+	  public static void main(String[] args) {          
+	        String s1 = "This is indexOf method";         
+	        // Passing char and index from  
+	        int index = s1.indexOf('e', 12); //Returns the index of this char  
+	        System.out.println("index of char "+index);       
+	    }  
+}
+/*
+index of substring 16
+index of substring -1
+
+*/
