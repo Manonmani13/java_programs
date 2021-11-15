b9fe0e14c804714d5aee7e0c6140863d553471a9
manonmani <manonmani.pandukumar@gmail.com>
2021-11-15 19:28:04 +0530

    String methods

---
+++ StringMethos_2/Trim1.java
@@ -0,0 +1,9 @@
+package StringMethos_2;
+
+public class Trim1 {
+public static void main(String[] args) {
+	String s="     krishna      hii   ";
+	System.out.println(s.trim());
+}
+}
+//krishna      hii
