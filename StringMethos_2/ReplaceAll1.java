99aae9f9fbdf0b9feffdd6464e98880ee3720a41
manonmani <manonmani.pandukumar@gmail.com>
2021-11-15 19:14:26 +0530

    String methods

---
+++ StringMethos_2/ReplaceAll1.java
@@ -0,0 +1,9 @@
+package StringMethos_2;
+
+public class ReplaceAll1 {
+public static void main(String[] args) {
+	String s="hello java readers";
+	System.out.println(s.replaceAll("e", "r"));
+}
+}
+//hrllo java rradrrs
