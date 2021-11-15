0e32f3f2cea6949311e1b5765cabd56f9d088a25
manonmani <manonmani.pandukumar@gmail.com>
2021-11-15 18:02:17 +0530

    String methods

---
+++ StringMethos_2/Join2.java
@@ -0,0 +1,11 @@
+package StringMethos_2;
+
+public class Join2 {
+    public static void main(String[] args) {          
+        String date = String.join("/","25","06","2018");    
+        System.out.print(date);    
+        String time = String.join(":", "12","10","10");  
+        System.out.println(" "+time);  
+    }  
+}
+//25/06/2018 12:10:10
