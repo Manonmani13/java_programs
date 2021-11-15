ddf9ec24a763a0ada4147ece71337ae91408c338
manonmani <manonmani.pandukumar@gmail.com>
2021-11-15 19:27:12 +0530

    String methods

---
+++ StringMethos_2/ToUpperCase2.java
@@ -0,0 +1,10 @@
+package StringMethos_2;
+
+public class ToUpperCase2 {
+	public static void main(String args[]){  
+		String s1="hello string";  
+		String s1upper=s1.toUpperCase();  
+		System.out.println(s1upper);  
+		}
+}
+//HELLO STRING
