f035514a3f96b0fe2ed4714a57f045a5ea9e2d68
manonmani <manonmani.pandukumar@gmail.com>
2021-11-15 17:44:02 +0530

    String methods

---
+++ StringMethodsW3/ContentEquls.java
@@ -0,0 +1,12 @@
+package StringMethodsW3;
+
+public class ContentEquls {
+public static void main(String[] args) {
+	String j="kirthu";
+	System.out.println(j.contentEquals("hii"));
+	System.out.println(j.contentEquals("u"));
+}
+}
+/*
+searches a string to find out if it contains the exact same sequence of characters in the specified string or StringBuffer.
+*/
