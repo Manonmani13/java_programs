df4cd20b277458548afda2c9c003f15cc48ba527
manonmani <manonmani.pandukumar@gmail.com>
2021-11-15 22:22:48 +0530

    Pattern

---
+++ Pattern_Pro/Patt6.java
@@ -0,0 +1,25 @@
+package Pattern_Pro;
+
+public class Patt6 {
+public static void main(String[] args) {
+	 for (int j = 1; j<= 6; j++) 
+     {
+         for (int i = j; i >= 1; i--)
+         {
+             System.out.print(i+" ");
+         }
+           
+         System.out.println();
+     }
+      
+}
+}
+/*
+1 
+2 1 
+3 2 1 
+4 3 2 1 
+5 4 3 2 1 
+6 5 4 3 2 1 
+
+*/
