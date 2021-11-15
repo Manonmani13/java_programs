441d57b5641779e979115ae2300b0df717021744
manonmani <manonmani.pandukumar@gmail.com>
2021-11-15 17:22:28 +0530

    String methods

---
+++ StringMethodsW3/Concat.java
@@ -0,0 +1,10 @@
+package StringMethodsW3;
+
+public class Concat {
+public static void main(String[] args) {
+	String s="manonmani";
+	String s1="pandukumar";
+	System.out.println(s.concat(s1));
+}
+}
+//manonmanipandukumar
