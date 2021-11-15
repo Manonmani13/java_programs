42c056f9ed1475373a429502e0244d99409a5f85
manonmani <manonmani.pandukumar@gmail.com>
2021-11-15 17:20:54 +0530

    String methods

---
+++ StringMethodsW3/Comapare.java
@@ -0,0 +1,15 @@
+package StringMethodsW3;
+
+public class Comapare {
+	  public static void main(String[] args) {
+		    String myStr1 = "Hello";
+		    String myStr2 = "Hello";
+		    String my="mano";
+		    System.out.println(my.compareTo(myStr2));
+		    System.out.println(myStr1.compareTo(myStr2)); // Returns 0 because they are equal
+		  }
+}
+/*
+37
+0
+*/
