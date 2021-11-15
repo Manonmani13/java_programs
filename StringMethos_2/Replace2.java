0abb3de9a29018ba922bbd9310078129bbc88025
manonmani <manonmani.pandukumar@gmail.com>
2021-11-15 19:12:57 +0530

    String methods

---
+++ StringMethos_2/Replace2.java
@@ -0,0 +1,9 @@
+package StringMethos_2;
+
+public class Replace2 {
+public static void main(String[] args) {
+	String v="welome to Panasonic";
+	System.out.println(v.replace("to","you all to"));
+}
+}
+//welome you all to Panasonic
