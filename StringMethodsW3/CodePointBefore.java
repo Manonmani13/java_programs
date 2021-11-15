8685778a91c861ef750229aa621217502948d699
manonmani <manonmani.pandukumar@gmail.com>
2021-11-15 17:15:04 +0530

    String methods

---
+++ StringMethodsW3/CodePointBefore.java
@@ -0,0 +1,10 @@
+package StringMethodsW3;
+
+public class CodePointBefore {
+	public static void main(String[] args) {
+	    String myStr = "Hallo";
+	    int result = myStr.codePointBefore(2);
+	    System.out.println(result);
+	  }
+}
+//97
