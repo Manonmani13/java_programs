a8d5ce8e821a63f31ade6ca8a19a8e770af417cb
manonmani <manonmani.pandukumar@gmail.com>
2021-11-15 17:46:15 +0530

    String methods

---
+++ StringMethodsW3/EndsWith.java
@@ -0,0 +1,13 @@
+package StringMethodsW3;
+
+public class EndsWith {
+public static void main(String[] args) {
+	String s="manonmani";
+	System.out.println(s.endsWith("i"));
+	System.out.println(s.endsWith("k"));
+}
+}
+/*
+true
+false
+*/
