df7bc64f112c8564f3373fa19a873743f867dcdc
manonmani <manonmani.pandukumar@gmail.com>
2021-11-15 18:08:51 +0530

    String methods

---
+++ StringMethos_2/Join3.java
@@ -0,0 +1,11 @@
+package StringMethos_2;
+
+public class Join3 {
+	// main method  
+	public static void main(String argvs[])  
+	{  
+	String str = null;  
+	str = String.join(null, "abc", "bcd", "apple");  
+	System.out.println(str);  
+	}  
+}
