8dea7e19528f571e1347ae9d5b4254f5fd8c0272
manonmani <manonmani.pandukumar@gmail.com>
2021-11-15 19:09:46 +0530

    String methods

---
+++ StringMethos_2/Length4.java
@@ -0,0 +1,19 @@
+package StringMethos_2;
+
+public class Length4 {
+	public static void main(String argvs[])  
+	{  
+	String str = " Welcome To JavaTpoint ";  
+	int sizeWithWhiteSpaces = str.length();  
+	  
+	System.out.println("In the string: " + "'" + str + "'");  
+	  
+	str = str.replace(" ", "");  
+	int sizeWithoutWhiteSpaces = str.length();  
+	  
+	// calculating the white spaces  
+	int noOfWhieSpaces = sizeWithWhiteSpaces - sizeWithoutWhiteSpaces;  
+	  
+	System.out.print("Total number of whitespaces present are: " + noOfWhieSpaces);  
+	}  
+}
