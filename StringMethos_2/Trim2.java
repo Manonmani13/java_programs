e30fe9a9de9a977d975e7eda3417ce08b33fbb10
manonmani <manonmani.pandukumar@gmail.com>
2021-11-15 19:28:21 +0530

    String methods

---
+++ StringMethos_2/Trim2.java
@@ -0,0 +1,15 @@
+package StringMethos_2;
+
+public class Trim2 {
+	public static void main(String[] args) {
+		
+	
+	String s1="  hello string   ";  
+	System.out.println(s1+"javatpoint");//without trim()  
+	System.out.println(s1.trim()+"javatpoint");//with trim()  
+	}
+}
+/*
+  hello string   javatpoint
+hello stringjavatpoint
+*/
