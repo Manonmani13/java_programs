47cc3708f888f07a258ed9971bce0f449dc45a44
manonmani <manonmani.pandukumar@gmail.com>
2021-11-15 19:20:02 +0530

    String methods

---
+++ StringMethos_2/StarsWith3.java
@@ -0,0 +1,17 @@
+package StringMethos_2;
+
+public class StarsWith3 {
+public static void main(String[] args) {
+	String str = "Tokyo Olympics";  
+	  
+	if(str.startsWith(""))  
+	{  
+	System.out.println("The string starts with the empty string.");  
+	}  
+	else  
+	{  
+	System.out.println("The string does not start with the empty string.");     
+	}  
+}
+}
+//The string starts with the empty string.
