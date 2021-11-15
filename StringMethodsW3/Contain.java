e7557158332949d68f3cc05d1a22ca82b12be791
manonmani <manonmani.pandukumar@gmail.com>
2021-11-15 17:22:54 +0530

    String methods

---
+++ StringMethodsW3/Contain.java
@@ -0,0 +1,13 @@
+package StringMethodsW3;
+
+public class Contain {
+public static void main(String[] args) {
+	String s="manonmani";
+	System.out.println(s.contains("m"));
+	System.out.println(s.contains("k"));
+}
+}
+/*
+true
+false
+*/
