c005fa62c14da992f695af0ed4892d351740026a
manonmani <manonmani.pandukumar@gmail.com>
2021-11-15 19:30:29 +0530

    String methods

---
+++ StringMethos_2/ValueOf1.java
@@ -0,0 +1,12 @@
+package StringMethos_2;
+
+public class ValueOf1 {
+	public static void main(String args[]){  
+		int value=30;  
+		String s1=String.valueOf(value);  
+		System.out.println(s1+10);//concatenating string with 10  
+		
+		}
+	
+}
+//3010
