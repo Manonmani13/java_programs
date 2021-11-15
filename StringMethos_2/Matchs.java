deff48be9a00d6c9f8fc678d2dadd7d467b0b8cf
manonmani <manonmani.pandukumar@gmail.com>
2021-11-15 19:10:16 +0530

    String methods

---
+++ StringMethos_2/Matchs.java
@@ -0,0 +1,11 @@
+package StringMethos_2;
+
+public class Matchs {
+public static void main(String[] args) {
+	String s3=" ";
+	String s4=" ";
+	System.out.println(s3.matches(s4));
+	//System.out.println(s3.);
+}
+}
+//true
