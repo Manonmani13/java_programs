cfee276cfeaa8114a34ab1bffdc426c8c2fe23f5
manonmani <manonmani.pandukumar@gmail.com>
2021-11-15 18:32:07 +0530

    String methods

---
+++ StringMethos_2/Lastindexof3.java
@@ -0,0 +1,17 @@
+package StringMethos_2;
+
+public class Lastindexof3 {
+public static void main(String[] args) {
+	String h="manonmani hi";
+	System.out.println(h.lastIndexOf("ma",6));
+	System.out.println(h.lastIndexOf("hi",10));
+    String str = "This is last index of example";  
+    System.out.println(str.lastIndexOf("of", 10));
+}
+}
+/*
+5
+10
+-1
+
+*/
