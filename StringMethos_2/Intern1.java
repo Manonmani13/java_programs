b107e9287076eb400a9d6ff69276bc19ef9c84ca
manonmani <manonmani.pandukumar@gmail.com>
2021-11-15 17:58:54 +0530

    String methods

---
+++ StringMethos_2/Intern1.java
@@ -0,0 +1,16 @@
+package StringMethos_2;
+
+public class Intern1 {
+public static void main(String[] args) {
+	String s1=new String("hello");  
+	String s2="hello";  
+	String s3=s1.intern();//returns string from pool, now it will be same as s2  
+	System.out.println(s1==s2);//false because reference variables are pointing to different instance  
+	System.out.println(s2==s3);
+}
+}
+/*
+false
+true
+
+*/
