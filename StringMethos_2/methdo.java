969ba520a99419aa8d1a70df84a216dcb4be19f0
manonmani <manonmani.pandukumar@gmail.com>
2021-11-15 19:11:01 +0530

    String methods

---
+++ StringMethos_2/methdo.java
@@ -0,0 +1,12 @@
+package StringMethos_2;
+
+public class methdo {
+public static void main(String[] args) {
+	String s="mano";
+	String s3=" ";
+	StringBuffer s2=new StringBuffer("mano");
+	System.out.println(s.contentEquals("mano"));//check full values
+	System.out.println(s.contentEquals(s2));
+	
+}
+}
