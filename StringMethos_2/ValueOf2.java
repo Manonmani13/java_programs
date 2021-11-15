83e1582b6e71098fe12841426cb73ddee4cca795
manonmani <manonmani.pandukumar@gmail.com>
2021-11-15 19:30:44 +0530

    String methods

---
+++ StringMethos_2/ValueOf2.java
@@ -0,0 +1,22 @@
+package StringMethos_2;
+
+public class ValueOf2 {
+	   public static void main(String[] args) {          
+	        // Boolean to String  
+	        boolean bol = true;    
+	        boolean bol2 = false;    
+	        String s1 = String.valueOf(bol);    
+	        String s2 = String.valueOf(bol2);  
+	        System.out.println(s1);  
+	        System.out.println(s2);  
+	        byte b=20;
+	        String s=String.valueOf(b);
+	       System.out.println( b); 
+	    }  
+}
+/*
+true
+false
+20
+
+*/
