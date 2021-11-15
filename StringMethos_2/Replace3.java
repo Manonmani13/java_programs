3e59d1ee415d6b75ffebf2962d5f15db8b593332
manonmani <manonmani.pandukumar@gmail.com>
2021-11-15 19:13:48 +0530

    String methods

---
+++ StringMethos_2/Replace3.java
@@ -0,0 +1,16 @@
+package StringMethos_2;
+
+public class Replace3 {
+public static void main(String[] args) {
+	String s="welcome to this website hi all";
+	String rs=s.replace("e", "m");
+	System.out.println(rs);
+	  rs = rs.replace("w","h");
+	  System.out.println(rs);
+}
+}
+/*
+wmlcomm to this wmbsitm hi all
+hmlcomm to this hmbsitm hi all
+
+*/
