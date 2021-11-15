4001e57524f39d3f091fa630ff5721025be46951
manonmani <manonmani.pandukumar@gmail.com>
2021-11-15 19:15:19 +0530

    String methods

---
+++ StringMethos_2/ReplaceAll3.java
@@ -0,0 +1,10 @@
+package StringMethos_2;
+
+public class ReplaceAll3 {
+	public static void main(String args[]){  
+		String s1="My name is Khan. My name is Bob. My name is Sonoo.";  
+		String replaceString=s1.replaceAll("\\s","");  
+		System.out.println(replaceString);  
+		}
+}
+//MynameisKhan.MynameisBob.MynameisSonoo.
