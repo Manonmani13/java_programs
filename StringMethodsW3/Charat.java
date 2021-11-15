9434db3e447f23359667143c0cbb8f9a964ada55
manonmani <manonmani.pandukumar@gmail.com>
2021-11-15 17:08:43 +0530

    String methods

---
+++ StringMethodsW3/Charat.java
@@ -0,0 +1,9 @@
+package StringMethodsW3;
+
+public class Charat {
+	  public static void main(String[] args) {
+		    String myStr = "Manonmani";
+		    char result = myStr.charAt(0);//M
+		    System.out.println(result);
+		  }
+}
