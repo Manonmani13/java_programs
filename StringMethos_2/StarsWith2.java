ba80b33a4228f0ec93cb12b37519774307a4938e
manonmani <manonmani.pandukumar@gmail.com>
2021-11-15 19:18:54 +0530

    String methods

---
+++ StringMethos_2/StarsWith2.java
@@ -0,0 +1,15 @@
+package StringMethos_2;
+
+public class StarsWith2 {
+public static void main(String[] args) {
+	String name="manonmani";
+	System.out.println(name.startsWith("m"));
+	System.out.println(name.startsWith("n",2));
+	System.out.println(name.startsWith("j"));
+}
+}
+/*
+true
+true
+false
+*/
