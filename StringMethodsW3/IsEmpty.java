1fed5bcbafa30c5f8a363f8abea980eacc6963e1
manonmani <manonmani.pandukumar@gmail.com>
2021-11-15 17:48:25 +0530

    String methods

---
+++ StringMethodsW3/IsEmpty.java
@@ -0,0 +1,16 @@
+package StringMethodsW3;
+
+public class IsEmpty {
+public static void main(String[] args) {
+	String myStr1 = "Hello";
+	String myStr2 = "";
+	System.out.println(myStr1.isEmpty());
+	System.out.println(myStr2.isEmpty());
+}
+}
+/*
+ * false
+true
+
+ 
+*/
