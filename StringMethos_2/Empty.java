11d2c7e3194298c0768c855cff6c349dd0eaad14
manonmani <manonmani.pandukumar@gmail.com>
2021-11-15 17:53:08 +0530

    String methods

---
+++ StringMethos_2/Empty.java
@@ -0,0 +1,15 @@
+package StringMethos_2;
+
+public class Empty {
+	public static void main(String args[]){  
+		String s1="";  
+		String s2="javatpoint";  
+		  
+		System.out.println(s1.isEmpty());  
+		System.out.println(s2.isEmpty());  
+		}
+}
+/*
+true
+false
+*/
