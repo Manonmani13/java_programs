154f77b3dc9a25379dde98eacf0a420441a05ce6
manonmani <manonmani.pandukumar@gmail.com>
2021-11-15 17:48:48 +0530

    String methods

---
+++ StringMethodsW3/LastIndexOf.java
@@ -0,0 +1,10 @@
+package StringMethodsW3;
+
+public class LastIndexOf {
+public static void main(String[] args) {
+	String myStr = "Hello planet earth, you are a great planet.";
+	System.out.println(myStr.lastIndexOf("planet"));
+	  System.out.println(myStr.lastIndexOf("e", 5));
+}
+}
+//36 1
