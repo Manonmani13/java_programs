f342b3e4fe8457150939d241a88a800d584e89ce
manonmani <manonmani.pandukumar@gmail.com>
2021-11-15 19:22:13 +0530

    String methods

---
+++ StringMethos_2/TocharArray1.java
@@ -0,0 +1,14 @@
+package StringMethos_2;
+
+public class TocharArray1 {
+public static void main(String[] args) {
+	 
+		String s1="manonmani";  
+		char[] ch=s1.toCharArray();  
+		for(int i=0;i<ch.length;i++){  
+		System.out.print(ch[i]+"*");  
+		}  
+		
+}
+}
+//m*a*n*o*n*m*a*n*i*
