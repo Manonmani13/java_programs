42513dee57743c124eb8c65ad430902d2e34a079
manonmani <manonmani.pandukumar@gmail.com>
2021-11-15 17:19:35 +0530

    String methods

---
+++ StringMethodsW3/CompareToIgnoreCase.java
@@ -0,0 +1,9 @@
+package StringMethodsW3;
+
+public class CompareToIgnoreCase {
+public static void main(String[] args) {
+	String s="MANONMANI";
+	String s1="manonmani";
+	System.out.println(s.compareToIgnoreCase(s1));//0
+}
+}
