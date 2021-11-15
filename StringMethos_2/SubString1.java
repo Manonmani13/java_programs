c5c5438dc81191bcd13a2d2f8bdb8344c3b21c4b
manonmani <manonmani.pandukumar@gmail.com>
2021-11-15 19:20:29 +0530

    String methods

---
+++ StringMethos_2/SubString1.java
@@ -0,0 +1,15 @@
+package StringMethos_2;
+
+public class SubString1 {
+public static void main(String[] args) {
+	String s="manonmani";
+	System.out.println(s.substring(2,5));
+	System.out.println(s.substring(6));
+	System.out.println(s.subSequence(3, 6));
+}
+}
+/*
+non
+ani
+onm
+*/
