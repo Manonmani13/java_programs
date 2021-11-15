12f165f6f5eeef3d627869c366bc8b9d01d31c23
manonmani <manonmani.pandukumar@gmail.com>
2021-11-15 17:47:45 +0530

    String methods

---
+++ StringMethodsW3/IndexOF.java
@@ -0,0 +1,11 @@
+package StringMethodsW3;
+
+public class IndexOF {
+public static void main(String[] args) {
+	String myStr = "Hello planet earth, you are a great planet.";
+	System.out.println(myStr.indexOf("planet"));
+	  //String my = "Hello planet earth, you are a great planet.";
+	    System.out.println(myStr.indexOf("e", 5));
+}
+}
+//6 10
