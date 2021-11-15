d4c9b5713a7208690aa5c7e6f2493113216b971c
manonmani <manonmani.pandukumar@gmail.com>
2021-11-15 17:57:24 +0530

    String methods

---
+++ StringMethos_2/Indexof3.java
@@ -0,0 +1,40 @@
+package StringMethos_2;
+
+public class Indexof3 {
+	public static void main(String argvs[])   
+	{     
+	  
+	String str = "Welcome to JavaTpoint";      
+	int count = 0;  
+	int startFrom = 0;  
+	for(; ;)  
+	{  
+	  
+	int index = str.indexOf('o', startFrom);  
+	  
+	if(index >= 0)  
+	{  
+	// match found. Hence, increment the count  
+	count = count + 1;  
+	  
+	// start looking after the searched index  
+	startFrom = index + 1;  
+	}  
+	  
+	else  
+	{  
+	// the value of index is - 1 here. Therefore, terminate the loop  
+	break;  
+	}  
+	  
+	}  
+	  
+	System.out.println("In the String: "+ str);   
+	System.out.println("The 'o' character has come "+ count + " times");  
+	}
+}
+/*
+In the String: Welcome to JavaTpoint
+The 'o' character has come 3 times
+
+*/
