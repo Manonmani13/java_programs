e91065b4c9077be7fca38b5b08d5087351d95e92
manonmani <manonmani.pandukumar@gmail.com>
2021-11-15 17:49:13 +0530

    String methods

---
+++ StringMethodsW3/Length.java
@@ -0,0 +1,8 @@
+package StringMethodsW3;
+
+public class Length {
+public static void main(String[] args) {
+	String txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	System.out.println(txt.length());	
+}
+}//26
