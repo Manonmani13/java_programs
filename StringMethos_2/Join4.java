211673d376df7cee70cd9749824887a26d859eb2
manonmani <manonmani.pandukumar@gmail.com>
2021-11-15 18:09:07 +0530

    String methods

---
+++ StringMethos_2/Join4.java
@@ -0,0 +1,10 @@
+package StringMethos_2;
+
+public class Join4 {
+	public static void main(String argvs[])  
+	{  
+	String str = null;  
+	//str = String.join("India", null,0);  
+	System.out.println(str);  
+	}  
+}
