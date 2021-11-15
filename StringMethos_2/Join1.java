038a02130327d865db967b09d8a8c8af5b6d12f2
manonmani <manonmani.pandukumar@gmail.com>
2021-11-15 18:02:02 +0530

    String methods

---
+++ StringMethos_2/Join1.java
@@ -0,0 +1,9 @@
+package StringMethos_2;
+
+public class Join1 {
+	public static void main(String args[]){  
+		String joinString1=String.join("-","welcome","to","javatpoint");  
+		System.out.println(joinString1);  
+		}
+}
+//welcome-to-javatpoint
