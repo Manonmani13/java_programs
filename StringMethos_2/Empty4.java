7fe5550db5a477cf1f2f7a893c37cd1a146683b7
manonmani <manonmani.pandukumar@gmail.com>
2021-11-15 17:53:47 +0530

    String methods

---
+++ StringMethos_2/Empty4.java
@@ -0,0 +1,17 @@
+package StringMethos_2;
+
+public class Empty4 {
+	public static void main(String argvs[])  
+	{  
+	String str = null;  
+	if(str == null)  
+	{  
+	System.out.println("The string is null.");  
+	}  
+	else  
+	{  
+	System.out.println("The string is not null.");  
+	}  
+	}  
+}
+//The string is null.
