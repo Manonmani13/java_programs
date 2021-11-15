815f4387bbfcb24f248013023b0a2735f9c28439
manonmani <manonmani.pandukumar@gmail.com>
2021-11-15 22:23:17 +0530

    Pattern

---
+++ Pattern_Pro/Patt8.java
@@ -0,0 +1,38 @@
+package Pattern_Pro;
+
+public class Patt8 {
+public static void main(String[] args) {
+	 for(int j =5;j>=1;j--)
+	    {
+	        for(int i =1;i<=j;i++)
+	        {
+	            System.out.print(i+" ");
+	        }
+	    System.out.println();
+	    }
+	     
+	    for(int k =1;k<=5;k++)
+	    {
+	     
+	    for(int i =1;i<=k;i++)
+	    {
+	        System.out.print(i+" ");
+	    }
+	    System.out.println();
+	    }
+}
+}
+/*
+1 2 3 4 5 
+1 2 3 4 
+1 2 3 
+1 2 
+1 
+1 
+1 2 
+1 2 3 
+1 2 3 4 
+1 2 3 4 5 
+
+
+*/
