52d0e5d435303528fc699ca4f2f4c70ce45a3987
manonmani <manonmani.pandukumar@gmail.com>
2021-11-15 17:57:42 +0530

    String methods

---
+++ StringMethos_2/Indexof4.java
@@ -0,0 +1,11 @@
+package StringMethos_2;
+
+public class Indexof4 {
+    public static void main(String[] args) {  
+        String s1 = "This is indexOf method";         
+        // Passing Substring    
+        int index = s1.indexOf("method"); //Returns the index of this substring  
+        System.out.println("index of substring "+index);          
+    }  
+}
+//index of substring 16
