d6430ccdb54ecddad82c5947e9ba7b778918903c
manonmani <manonmani.pandukumar@gmail.com>
2021-11-15 19:25:40 +0530

    String methods

---
+++ StringMethos_2/ToUpperCase1.java
@@ -0,0 +1,9 @@
+package StringMethos_2;
+
+public class ToUpperCase1 {
+public static void main(String[] args) {
+	String name="manonmani";
+	System.out.println(name.toUpperCase());
+}
+}
+//MANONMANI
