d94b12cea9662d7eea9ea80ee99cf3ea8d466839
manonmani <manonmani.pandukumar@gmail.com>
2021-11-15 19:24:13 +0530

    String methods

---
+++ StringMethos_2/ToLowerChar1.java
@@ -0,0 +1,9 @@
+package StringMethos_2;
+
+public class ToLowerChar1 {
+public static void main(String[] args) {
+	String name="MANONMANI";
+	System.out.println(name.toLowerCase());
+}
+}
+//manonmani
