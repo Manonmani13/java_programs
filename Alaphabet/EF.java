915cabdfca0a78e3e81a52c09fdc29c843244a8d
manonmani <manonmani.pandukumar@gmail.com>
2021-11-15 22:06:53 +0530

    Alphabet

---
+++ Alaphabet/EF.java
@@ -0,0 +1,81 @@
+package Alaphabet;
+
+public class EF{
+public static void main(String[] args) {
+	for (int row = 1; row <= 9; row++) 
+    {
+if (row == 1) 
+        {
+for (int i = 1; i <= 7; i++) 
+            {
+    System.out.print("* ");
+    }
+} 
+        else if (row == 5) 
+        {
+for (int i = 1; i <= 7; i++)
+            {
+    System.out.print("* ");
+}
+} 
+        else if (row == 9) 
+        {
+for (int i = 1; i <= 7; i++) 
+            {
+    System.out.print("* ");
+}
+}
+        else
+        {
+System.out.print("*            ");
+}
+System.out.println();
+}
+System.out.println();
+// F
+for (int row = 1; row <= 9; row++) 
+    {
+if (row == 1) 
+        {
+for (int i = 1; i <= 7; i++)
+            {
+     System.out.print("* ");
+    }
+ } 
+         else if (row == 5) 
+         {
+for (int i = 1; i <= 7; i++) 
+            {
+    System.out.print("* ");
+}
+ }
+         else 
+         {
+System.out.print("*            ");
+ }
+     System.out.println();
+}
+}
+}
+/*
+* * * * * * * 
+*            
+*            
+*            
+* * * * * * * 
+*            
+*            
+*            
+* * * * * * * 
+
+* * * * * * * 
+*            
+*            
+*            
+* * * * * * * 
+*            
+*            
+*            
+*            
+
+*/
