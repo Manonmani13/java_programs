0c72269dcf00c56f66743a61d742de8086cf041b
manonmani <manonmani.pandukumar@gmail.com>
2021-11-16 22:31:36 +0530

    map

---
+++ Map/HashMap1.java
@@ -0,0 +1,17 @@
+package Map;
+
+import java.util.HashMap;
+
+public class HashMap1 {
+public static void main(String[] args) {
+	HashMap hm=new HashMap();
+	hm.put("Tamil mark",90);
+	hm.put("English mark",90);
+	hm.put("Maths marks",80);
+	hm.put("Science mark",100);
+	hm.put("Soial mark",98);
+	System.out.println(hm);
+	
+}
+}
+//{Soial mark=98, English mark=90, Science mark=100, Tamil mark=90, Maths marks=80}
