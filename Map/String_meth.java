3a81c16a4bb79c6e0b293c63968127e83cb9de49
manonmani <manonmani.pandukumar@gmail.com>
2021-11-16 22:35:37 +0530

    map

---
+++ Map/String_meth.java
@@ -0,0 +1,63 @@
+package Map;
+
+public class String_meth {
+	public static void main(String[] args) {
+	    // TODO Auto-generated method stub
+	    
+	    String s1 =  new String("you cannot change me");
+	    String s2 =  new String("you cannot change me");
+	    System.out.println(s1.hashCode());
+	    System.out.println(s2.hashCode());
+	    System.out.println(s1==s2);
+	    System.out.println(s1.hashCode());
+	    System.out.println(s2.hashCode());
+	    System.out.println();
+	    
+//	    String s3 = "you cannot change me";
+//	    System.out.println(s1==s3);
+//	    System.out.println(s1.hashCode());
+//	    System.out.println(s3.hashCode());
+//	    System.out.println();
+//	    
+//	    String s4 = "you cannot change me";
+//	    System.out.println(s3==s4);
+//	    System.out.println(s3.hashCode());
+//	    System.out.println(s4.hashCode());
+//	    System.out.println();
+//
+//	    String s5 = "you cannot"+"change me";
+//	    System.out.println(s4==s5);
+//	    System.out.println(s4.hashCode());
+//	    System.out.println(s5.hashCode());
+//	    System.out.println();
+//	    
+//	    String s6 ="you cannot";
+//	    String s7 =s6+"change me";
+//	    System.out.println(s3==s7);
+//	    System.out.println(s3.hashCode());
+//	    System.out.println(s6.hashCode());
+//	    System.out.println(s7.hashCode());
+//	    System.out.println();
+//	    
+//	    final String s8 ="you cannot";
+//	    String s9 = s8+"change me";
+//	    System.out.println(s4==s9);
+//	    System.out.println(s6==s8);
+//	    System.out.println(s8.hashCode());
+//	    System.out.println(s9.hashCode());
+//	        System.out.println();
+//	        
+//	        String s10 = s1;
+//	        System.out.println(s1==s10);
+//	        System.out.println(s1.hashCode());
+//	    System.out.println(s10.hashCode());
+//	    System.out.println();
+//	     
+//	        String s11 = s3;
+//	        System.out.println(s1==s10);
+//	        System.out.println(s1.hashCode());
+//	    System.out.println(s10.hashCode());
+	    
+	  }
+
+	}
