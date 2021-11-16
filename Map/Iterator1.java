16745dd0eac7a68a3aaad54b562eb6ec9cb3f14d
manonmani <manonmani.pandukumar@gmail.com>
2021-11-16 22:34:36 +0530

    map

---
+++ Map/Iterator1.java
@@ -0,0 +1,52 @@
+package Map;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+public class Iterator1 {
+	public static void main(String[] args) {
+		HashMap hm = new HashMap();
+		  hm.put("Tamil I", 9);
+		  hm.put("Tamil II", 9);
+		  hm.put("English I", 9);
+		  hm.put("English II", 9);
+		  hm.put("Mathematics", 2);
+		  hm.put("Science", 2);
+		  hm.put("Social Science", 2);
+		  
+		  System.out.println(hm);
+		  
+		  Set s = hm.keySet();
+		  System.out.println(s);
+		  
+		  Collection c= hm.values();
+		  System.out.println(c);
+		    
+		  Set s1 = hm.entrySet();
+		  Iterator i = s1.iterator(); 
+		  boolean b = i.hasNext();
+		  while(b==true)
+		  {
+		    
+		    System.out.println(i.next());
+		    b = i.hasNext();
+		  }
+		  
+		  
+	}
+}/*
+{English I=9, Tamil II=9, Science=2, Mathematics=2, Social Science=2, Tamil I=9, English II=9}
+[English I, Tamil II, Science, Mathematics, Social Science, Tamil I, English II]
+[9, 9, 2, 2, 2, 9, 9]
+English I=9
+Tamil II=9
+Science=2
+Mathematics=2
+Social Science=2
+Tamil I=9
+English II=9
+
+*/
