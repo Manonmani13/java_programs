48a47506669886240d961bfade5a2476aedbc324
manonmani <manonmani.pandukumar@gmail.com>
2021-11-18 21:52:13 +0530

    collection

---
+++ Collecction_18/ComparatorDemo.java
@@ -0,0 +1,23 @@
+package Collecction_18;
+
+import java.util.Comparator;
+
+public class ComparatorDemo implements Comparator{
+
+	@Override
+	public int compare(Object a, Object a1) {
+		// TODO Auto-generated method stub
+		Integer i=(Integer)a;
+		Integer i1=(Integer)a1;
+//		if(i>i1)
+//		{
+//			return +5;
+//		}
+//		else if(i<i1)
+//		{
+//			return -5;
+//		}
+		return 0;
+	}
+
+}
