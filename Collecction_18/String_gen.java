6758c2578e81bc4242d64a1aa41e4e0b2159f087
manonmani <manonmani.pandukumar@gmail.com>
2021-11-18 21:54:42 +0530

    collection

---
+++ Collecction_18/String_gen.java
@@ -0,0 +1,14 @@
+package Collecction_18;
+
+import java.util.ArrayList;
+
+public class String_gen {
+public static void main(String[] args) {
+	ArrayList<String> al=new ArrayList();
+	al.add("hi");
+	al.add("hello");
+	al.add("welcome");
+	System.out.println(al);
+}
+}
+//[hi, hello, welcome]s
