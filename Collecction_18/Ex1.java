37f450cd74f8a9a04c3c068ac120ec949bd22acd
manonmani <manonmani.pandukumar@gmail.com>
2021-11-18 21:53:06 +0530

    collection

---
+++ Collecction_18/Ex1.java
@@ -0,0 +1,15 @@
+package Collecction_18;
+
+public class Ex1 {
+	 public static void main(String[] args) {
+		ClEx c=new ClEx("abcd");
+		c.display();
+		ClEx c1=new ClEx(12.3);
+		c1.display();
+	}
+}
+/*
+abcd --> java.lang.String
+12.3 --> java.lang.Double
+
+*/
