f885a5b6b8ec884d519aa6c09b42f08d8baad3d8
manonmani <manonmani.pandukumar@gmail.com>
2021-11-21 19:02:31 +0530

    project

---
+++ Logical_Pro/App.java
@@ -0,0 +1,20 @@
+package com.example.Prorentention;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static <ApplicationContext> void main( String[] args )
+    {
+        //System.out.println( "Hello World!" );
+    	//Car car=new Car();
+    	//car.drive()
+ //ApplicationContext con=new ClassPathXmlApplicationContext("Spring.xml");
+   
+    	//Vehile o=(Vehile)con.getBean("vechile");
+    	//o.drive();
+    	
+    }
+}
