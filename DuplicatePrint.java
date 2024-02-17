6bedd4fd8f7dcf775b538d7e02ccbe0564d9e3c7
ManonmaniPandukumar <86237230+Manonmani13@users.noreply.github.com>
2024-02-17 23:33:22 +0530

    Add files via upload

---
+++ DuplicatePrint.java
@@ -0,0 +1,17 @@
+package example;
+
+public class DuplicatePrint {
+public static void main(String[] args) {
+	int a[]= {2,3,4,5,2,3,5,0,9,0};
+	for(int i=0;i<a.length;i++)
+	{
+		for(int j=i+1;j<a.length;j++)
+		{
+			if(a[i]==a[j])
+			{
+				System.out.println(a[j]);
+			}
+		}
+	}
+}
+}
