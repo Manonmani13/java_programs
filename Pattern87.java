8b77417621797307bc2ed41fd648edfef2a8ac28
ManonmaniPandukumar <86237230+Manonmani13@users.noreply.github.com>
2024-03-08 14:01:19 +0530

    Add files via upload

---
+++ Pattern87.java
@@ -0,0 +1,14 @@
+package patternapp;
+
+public class Pattern87 {
+	public static void main(String[] args) {
+		for(int i=1;i<=5;i++)
+		{
+			for(int j=i;j<=5;j++)
+			{
+				System.out.print((char)(i+64));
+			}
+			System.out.println();
+		}
+	}
+}
