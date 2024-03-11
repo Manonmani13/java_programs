8c805684df7761c9962b8ea255396d41842b51e3
ManonmaniPandukumar <86237230+Manonmani13@users.noreply.github.com>
2024-03-11 13:33:22 +0530

    Add files via upload

---
+++ Pattern95.java
@@ -0,0 +1,19 @@
+package patternapp;
+
+public class Pattern95 {
+	public static void main(String[] args) {
+	
+		for(int i=1;i<=5;i++)
+		{
+			for(int j=i;j<=4;j++)
+			{
+				System.out.print(" ");
+			}
+			for(int j=1;j<=i;j++)
+			{
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
