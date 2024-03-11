6a412c98afd14c6036cf93205ad8c41d6f4bf170
ManonmaniPandukumar <86237230+Manonmani13@users.noreply.github.com>
2024-03-11 13:18:39 +0530

    Add files via upload

--- Pattern92.java
+++ Pattern92.java
@@ -2,14 +2,12 @@ package patternapp;
 
 public class Pattern92 {
 	public static void main(String[] args) {
-		int k=5;
-		for(int i=0;i<=4;i++)
+		for(int i=1;i<=5;i++)
 		{
-			for(int j=k;j>i;j--)
+			for(int j=5;j>=i;j--)
 			{
-				System.out.print((char)(j+64));
+				System.out.print((char)(i+j-1+64));
 			}
-			k++;
 			System.out.println();
 		}
 	}
