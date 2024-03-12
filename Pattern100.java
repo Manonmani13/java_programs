1ed3181c90f5d0eb90264551078b13d63ea220ab
ManonmaniPandukumar <86237230+Manonmani13@users.noreply.github.com>
2024-03-12 18:43:22 +0530

    Add files via upload

---
+++ Pattern100.java
@@ -0,0 +1,19 @@
+package patternapp;
+
+public class Pattern100 {
+public static void main(String[] args) {
+		
+		for(int i=1;i<=5;i++)
+		{
+			for(int j=i;j<=4;j++)
+			{
+				System.out.print(" ");
+			}
+			for(int j=i;j>=1;j--)
+			{
+				System.out.print(j);
+			}
+			System.out.println();
+		}
+	}
+}
